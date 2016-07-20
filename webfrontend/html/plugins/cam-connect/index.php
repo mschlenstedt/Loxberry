4151eef93aa1ed38aa962819cfee0855abd8cf0b
Wörsty <git@loxberry.woerstenfeld.de>
2016-07-20 08:59:47 +0200

    Cam-Plugin Multilanguage support
    Cam-Plugin Multilanguage support

--- webfrontend/html/plugins/cam-connect/index.php
+++ webfrontend/html/plugins/cam-connect/index.php
@@ -1,44 +1,65 @@
 <?php
 #####################################################################################################
-# Loxberry Plugin zum Wandeln der HTTP-Authentifizierung einer Trendnet TV-IP310PI Kamera von       
-# Digest auf Basic für die Verwendung in Verbindung mit dem Loxone Türsteuerungs-Baustein.          
-# Version: 18.07.2016 16:44:25                                                                      
-# Aufruf mit:                                                                                       
-# http://loxberry/plugins/cam-connect/?kamera=kamera-vorgarten/Streaming/channels/1/picture&user=Loxberry&pass=loxberry 
+# Loxberry Plugin to change the HTTP-Authentication of a Trendnet TV-IP310PI Surveillance IP-Cam
+# from Digest to Basic to be used in the Loxone Door-Control-Object.          
+# Version: 20.07.2016 07:47:37
+# Call via:                                                                                       
+# http://loxberry/plugins/cam-connect/?kamera=Kamera-Hostname:80/Streaming/channels/1/picture&user=Benutzername&pass=Passwort 
 #####################################################################################################
 
-//Error Reporting aus
+// Error Reporting off
 error_reporting(E_ALL);
 
-//Kompletter Standbild-URL der Kamera
-$url='http://'.addslashes($_GET['kamera']); 
-$user='http://'.addslashes($_GET['user']); 
-$pass='http://'.addslashes($_GET['pass']); 
+// Read LoxBerry configuration file to get the used language
+$config_file = "../../../../config/system/general.cfg";
+$cfg         = parse_ini_file($config_file, TRUE);
+$lang        = $cfg["BASE"]["LANG"];
+// $lang="en";  // To override the Loxberry Base-Language uncomment this line.
+
+// Read Plugin configuration file to get the strings for the right language
+$phrase_file = "../../../../templates/plugins/cam-connect/$lang/language.dat";
+$phrases     = parse_ini_file($phrase_file);
+$error_msg   = $phrases["TXT0001"];
+(strlen($error_msg) > 0)?$error_msg=$error_msg:$error_msg="Plugin-Error: Errortext N/A";   
+
+// Read IP-CAM connection details from URL 
+$url  = 'http://'.addslashes($_GET['kamera']); 
+$user = 'http://'.addslashes($_GET['user']); 
+$pass = 'http://'.addslashes($_GET['pass']); 
+
+// Wait a second to be sure the image is available.
 sleep(1);
-$curl=curl_init();
+
+// Init and config cURL
+$curl = curl_init();
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST);
 curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
 curl_setopt($curl, CURLOPT_USERPWD, $_GET['user'].":".$_GET['pass']);
 curl_setopt($curl, CURLOPT_URL, $url);
+
+// Read picture from IP-Cam and close connection to Cam
 $picture = curl_exec($curl);
 curl_close($curl);
+
+// If the result has less than 500 byte, it's no picture.
 if(mb_strlen($picture) < 500) 
 {
-  //Wenn Bild zu klein
+  // Display an Error-Picture instead.
   header ("Content-type: image/png");
-  $im = @ImageCreate (320, 240)
-        or die ("Fehler bei Kamerazugriff");
-  $background_color = ImageColorAllocate ($im, 255, 240, 240);
-  $text_color = ImageColorAllocate ($im, 255, 64, 64);
-  ImageString ($im, 20, 50, 110, "Fehler beim Kamerazugriff", $text_color);
-  ImagePNG ($im);
+  $image = @ImageCreate (320, 240) or die ($error_msg);
+  $background_color = ImageColorAllocate ($image, 255, 240, 240);
+  $text_color = ImageColorAllocate ($image, 255, 64, 64);
+  ImageString ($image, 20, 50, 110, $error_msg, $text_color);
+  ImagePNG ($image);
+  ImageDestroy($image);
 }
 else 
 {
-  //Wenn Bild scheinbar okay
-  $type = 'image/jpeg';
-  header('Content-Type:'.$type);
-  //header('Content-Length: ' . mb_strlen($picture));
+  // Seems to be ok - Display the received picture.
+  list($width, $height, $type, $attr) = getimagesizefromstring($picture);
+  header('Content-Type: '.$type);
+  header('Content-length: '.strlen($picture));
+  header('Content-Disposition: inline; filename="snapshot.jpg"');
   echo $picture;
 }
