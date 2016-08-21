ebff85564bd0f1a2d8d38f198d052c7456c3cac1
Michael Schlenstedt <michael@loxberry.de>
2016-08-21 06:10:16 +0200

    Bugfixes

--- sbin/loxberryinit.sh
+++ sbin/loxberryinit.sh
@@ -69,6 +69,7 @@ case "$1" in
 
         # Set Date and Time
         if [ -f /opt/loxberry/sbin/setdatetime.pl ]
+        then
 	  log_action_begin_msg "Syncing Date/Time with Miniserver or NTP-Server"
           /opt/loxberry/sbin/setdatetime.pl > /dev/null 2>&1
         fi
