8bc006c809cd99ad45abe6a754601520ff09b848
Michael Schlenstedt <michael@loxberry.de>
2016-08-30 12:14:06 +0200

    Mailserver support

---
+++ sbin/createssmtpconf.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# What to do
+case "$1" in
+
+start)
+
+  if [ -f /tmp/tempssmtpconf.dat ] && [ -f /opt/loxberry/system/ssmtp/ssmtp.conf ]; then
+    # Backup old file
+    mv /opt/loxberry/system/ssmtp/ssmtp.conf /opt/loxberry/system/ssmtp/ssmtp.conf.bkp
+    chmod 600 /opt/loxberry/system/ssmtp/ssmtp.conf.bkp
+
+    # Copy new file
+    chmod 600 /tmp/tempssmtpconf.dat
+    cp /tmp/tempssmtpconf.dat /opt/loxberry/system/ssmtp/ssmtp.conf
+    chmod 600 /opt/loxberry/system/ssmtp/ssmtp.conf
+    rm  /tmp/tempssmtpconf.dat
+  fi
+  ;;
+
+stop)
+
+  if [ -f /opt/loxberry/system/ssmtp/ssmtp.conf.bkp ] && [ -f /opt/loxberry/system/ssmtp/ssmtp.conf ]; then
+    # Re-Create old file
+    mv /opt/loxberry/system/ssmtp/ssmtp.conf.bkp /opt/loxberry/system/ssmtp/ssmtp.conf
+    chmod 600 /opt/loxberry/system/ssmtp/ssmtp.conf
+  fi
+  ;;
+
+*)
+  echo "Use start/stop as parameter"
+  ;;
+
+esac
