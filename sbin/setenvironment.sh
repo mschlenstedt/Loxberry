dfb93a9a87ceb35e0fb80038b06e51cf424bb63c
christianTF <fenzl@t-r-t.at>
2017-11-04 21:15:08 +0100

    Environments and library pathes
    - Moved perllib to subdirectory in /opt/loxberry/libs
    - Created /opt/loxberry/phplib directory and a test php file
    - setenvironemt.sh: Include Env to Apache (envvars)
    - setenvironemt.sh: Set include_path in PHP5

--- sbin/setenvironment.sh
+++ sbin/setenvironment.sh
@@ -1,24 +1,48 @@
 #!/bin/bash
-HOME="/opt/loxberry"
+LBHOME="/opt/loxberry"
 
-echo LBHOMEDIR=$HOME >> /etc/environment
+echo LBHOMEDIR=$LBHOME >> /etc/environment
 
 # Main directories for plugins
-echo LBPCGI=$HOME/webfrontend/cgi/plugins >> /etc/environment
-echo LBPHTML=$HOME/webfrontend/html/plugins >> /etc/environment
-echo LBPTEMPL=$HOME/templates/plugins >> /etc/environment
-echo LBPDATA=$HOME/data/plugins >> /etc/environment
-echo LBPLOG=$HOME/log/plugins >> /etc/environment
-echo LBPCONFIG=$HOME/config/plugins >> /etc/environment
+echo LBPCGI=$LBHOME/webfrontend/cgi/plugins >> /etc/environment
+echo LBPHTML=$LBHOME/webfrontend/html/plugins >> /etc/environment
+echo LBPTEMPL=$LBHOME/templates/plugins >> /etc/environment
+echo LBPDATA=$LBHOME/data/plugins >> /etc/environment
+echo LBPLOG=$LBHOME/log/plugins >> /etc/environment
+echo LBPCONFIG=$LBHOME/config/plugins >> /etc/environment
 
 # Main directories for system
-echo LBSCGI=$HOME/webfrontend/cgi/system >> /etc/environment
-echo LBSHTML=$HOME/webfrontend/html/system >> /etc/environment
-echo LBSTEMPL=$HOME/templates/system >> /etc/environment
-echo LBSDATA=$HOME/data/system >> /etc/environment
-echo LBSLOG=$HOME/log/system >> /etc/environment
-echo LBSCONFIG=$HOME/config/system >> /etc/environment
+echo LBSCGI=$LBHOME/webfrontend/cgi/system >> /etc/environment
+echo LBSHTML=$LBHOME/webfrontend/html/system >> /etc/environment
+echo LBSTEMPL=$LBHOME/templates/system >> /etc/environment
+echo LBSDATA=$LBHOME/data/system >> /etc/environment
+echo LBSLOG=$LBHOME/log/system >> /etc/environment
+echo LBSCONFIG=$LBHOME/config/system >> /etc/environment
+
+# LoxBerry global environment variables in Apache
+ENVVARS=$LBHOME/system/apache2/envvars
+echo '' >> $ENVVARS
+echo '## LoxBerry global environment variables' >> $ENVVARS
+echo export LBHOMEDIR=$LBHOMEDIR >> $ENVVARS
+echo export LBPCGI=$LBPCGI >> $ENVVARS
+echo export LBPHTML=$LBPHTML >> $ENVVARS
+echo export LBPTEMPL=$LBPTEMPL >> $ENVVARS
+echo export LBPDATA=$LBPDATA >> $ENVVARS
+echo export LBPLOG=$LBPLOG >> $ENVVARS
+echo export LBPCONFIG=$LBPCONFIG >> $ENVVARS
+echo '' >> $ENVVARS
+echo export LBSCGI=$LBSCGI >> $ENVVARS
+echo export LBSHTML=$LBSHTML >> $ENVVARS
+echo export LBSTEMPL=$LBSTEMPL >> $ENVVARS
+echo export LBSDATA=$LBSDATA >> $ENVVARS
+echo export LBSLOG=$LBSLOG >> $ENVVARS
+echo export LBSCONFIG=$LBSCONFIG >> $ENVVARS
 
 # Set Perl library path for LoxBerry Modules
 
-echo PERL5LIB=$HOME/perllib  >> /etc/environment
+echo PERL5LIB=$LBHOME/libs/perllib  >> /etc/environment
+
+# Set PHP include_path directive
+
+echo include_path=\".:$LBHOME/libs/phplib\" > /etc/php5/apache2/conf.d/20-loxberry.ini
+echo include_path=\".:$LBHOME/libs/phplib\" > /etc/php5/cli/conf.d/20-loxberry.ini
