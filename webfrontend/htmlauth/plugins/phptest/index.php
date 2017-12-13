a062feef818969fb702c53c9e403f85bf0628c95
Christian Fenzl <fenzl@t-r-t.at>
2017-12-13 01:27:59 +0100

    Enhanced PHPTEST example

--- webfrontend/htmlauth/plugins/phptest/index.php
+++ webfrontend/htmlauth/plugins/phptest/index.php
@@ -3,22 +3,37 @@
 require_once "loxberry_system.php";
 require_once "loxberry_web.php";
 
+$L = Web::readlanguage("language.ini");
 
 $template_title = "Hello PHP! Plugin";
 $helplink = "http://www.loxwiki.eu:80/x/_wFmAQ";
 $helptemplate = "pluginhelp.html";
 
-Web::head();
-Web::pagestart($template_title, $helplink, $helptemplate);
+Web::lbheader($template_title, $helplink, $helptemplate);
+?>
 
+<h1><?=$L['MAIN.TITLE']?></h1>
+<p><?=$L['MAIN.INTRO']?></p>
+<p><?=$L['MAIN.USAGE1']?></p>
+<p><?=$L['MAIN.USAGE2']?></p>
+<p><?=$L['MAIN.HELP']?></p>
+<p></p>
+<p><b><?=$L['MAIN.FINALMESSAGE']?></b></p>
+<h2><?=$L['SIGNATURE.SIGN']?></h2>
+<p class="wide"><?=$L['SIGNATURE.SLOGAN']?></p>
+<p></p>
+
+<?php 
 echo "LoxBerry Web Lib Version is " . Web::$LBWEBVERSION . "<br>";
 echo "Language: " . Web::lblanguage();
-echo LoxBerry\System\is_systemcall();
-// Web::readlanguage();
-Web::pageend();
-Web::foot();
+?>
 
+<hr>
+<p class="hint"><?=$L['CODE.INTRO']?></p>
+<div class="monospace">
+	<p><?=str_replace("\n", "<br>", htmlspecialchars(file_get_contents("$LBHTMLAUTHDIR/index.php")))?></p>
+</div>
 
-
-
+<?php
+Web::lbfooter();
 ?>
