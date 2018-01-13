d4580c04293dbb2f96a28adccf15fcf5f73e08dd
Christian Fenzl <fenzl@t-r-t.at>
2018-01-13 18:02:59 +0100

    loxberry_log.php Stub
    No, it's not finished ;-)

---
+++ libs/phplib/loxberry_log.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once "loxberry_system.php";
+// require_once "phphtmltemplate_loxberry/template040.php";
+
+class LBLog
+{
+	public static $VERSION = "0.3.3.1";
+	
+	function get_notifications ($package = NULL, $name = NULL, $latest = NULL, $count = NULL, $getcontent = NULL)
+	{
+		error_log "get_notifications called.\n";
+		
+		
+	}
+
+	
+}
+
+
+?>
