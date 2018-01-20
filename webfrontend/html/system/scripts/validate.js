a54d24cb542738c713ee0c191dd6351f75292448
Wörsty <git@loxberry.woerstenfeld.de>
2018-01-20 01:03:08 +0100

    Validate: Mailserver
    -Except username/pass

--- webfrontend/html/system/scripts/validate.js
+++ webfrontend/html/system/scripts/validate.js
@@ -264,6 +264,11 @@ function validate_chk_value( object,evt,rule )
 				// Check if min + max digits values are given otherwise default to 1
 				rule = ( parseInt($(object).val())  >= parseInt(rule_array[2]) && parseInt($(object).val())  <= parseInt(rule_array[3])  ) ? '^([\-\+][0-9]|[0-9])*$' : '(?=x)y';
 				break;
+			case 'email':
+				// Check if email 
+				// (?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])
+				rule ='^(([^<>()\\[\\]\\\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$';
+				break;
 			case 'hostname':
 				// Check if hostname following RFC1123
 				rule ='^(?![0-9]+$)(?!.*-$)(?!-)[a-zA-Z0-9-]{1,63}$';
@@ -331,6 +336,9 @@ function validate_chk_value( object,evt,rule )
 				$('#error-msg-'+object.substring(1)).css({'z-index':1000, 'margin': '-1px', 'margin-top':'auto', 'padding': '5px', 'border': '1px solid #FF0000', 'border-radius': '0px 0px 8px 8px', 'color': '#FF0000', 'background-color': '#FFFFC0', 'white-space': 'normal'});
 				$(".ui-input-text").css("margin-bottom",0);
 				$('#error-msg-'+object.substring(1)).css('margin-top','-1px');
+
+				// Remove the bottom round corners of input to connect the error message 
+				$(object).css({'border-radius': '8px 8px 0px 0px'});
 
 				// Show the tooltip
 				$('#error-msg-'+object.substring(1)).fadeIn(500);
