34461693594e94ec64d0c23e557f9ff821316d33
Svethi <svethi@loxberry.de>
2018-01-26 21:09:25 +0100

    Debug-Test Bash Log

--- libs/bashlib/loxberry_log.sh
+++ libs/bashlib/loxberry_log.sh
@@ -4,114 +4,64 @@ function LOGDEB {
 	if [ "$LOGLEVEL" -gt 6 ]
 	then
 	  if [ -n "$ADDTIME" ];then CURRTIME=$(date +"%H:%M:%S ");else CURRTIME=""; fi
-		if [ -n "$NOFILE" ];then
-			echo "<DEBUG> $CURRTIME$@"
-		elif [ -n "$STDERR" ] || [ -z ${FILENAME+x} ];then
-			echo "<DEBUG> $CURRTIME$@" 1>&2
-		else
-			echo "<DEBUG> $CURRTIME$@" >> $FILENAME
-		fi	fi
+		WRITE "<DEBUG> $CURRTIME$@"
+	fi
 }
 function LOGINF { 
 	if [ "$LOGLEVEL" -gt 5 ]
 	then
 		if [ -n "$ADDTIME" ];then CURRTIME=$(date +"%H:%M:%S ");else CURRTIME=""; fi
-		if [ -n "$NOFILE" ];then
-			echo "<INFO> $CURRTIME$@"
-		elif [ -n "$STDERR" ] || [ -z ${FILENAME+x} ];then
-			echo "<INFO> $CURRTIME$@" 1>&2
-		else
-			echo "<INFO> $CURRTIME$@" >> $FILENAME
-		fi
+		WRITE "<INFO> $CURRTIME$@"
 	fi
 }
 function LOGOK { 
 	if [ "$LOGLEVEL" -gt 4 ]
 	then
 		if [ -n "$ADDTIME" ];then CURRTIME=$(date +"%H:%M:%S ");else CURRTIME=""; fi
-		if [ -n "$NOFILE" ];then
-			echo "<OK> $CURRTIME$@"
-		elif [ -n "$STDERR" ] || [ -z ${FILENAME+x} ];then
-			echo "<OK> $CURRTIME$@" 1>&2
-		else
-			echo "<OK> $CURRTIME$@" >> $FILENAME
-		fi
+		WRITE "<OK> $CURRTIME$@"
 	fi
 }
 function LOGWARN { 
 	if [ "$LOGLEVEL" -gt 3 ]
 	then
 		if [ -n "$ADDTIME" ];then CURRTIME=$(date +"%H:%M:%S ");else CURRTIME=""; fi
-		if [ -n "$NOFILE" ];then
-			echo "<WARNING> $CURRTIME$@"
-		elif [ -n "$STDERR" ] || [ -z ${FILENAME+x} ];then
-			echo "<WARNING> $CURRTIME$@" 1>&2
-		else
-			echo "<WARNING> $CURRTIME$@" >> $FILENAME
-		fi
+		WRITE "<WARNING> $CURRTIME$@"
 	fi
 }
 function LOGERR { 
 	if [ "$LOGLEVEL" -gt 2 ]
 	then
 		if [ -n "$ADDTIME" ];then CURRTIME=$(date +"%H:%M:%S ");else CURRTIME=""; fi
-		if [ -n "$NOFILE" ];then
-			echo "<ERROR> $CURRTIME$@"
-		elif [ -n "$STDERR" ] || [ -z ${FILENAME+x} ];then
-			echo "<ERROR> $CURRTIME$@" 1>&2
-		else
-			echo "<ERROR> $CURRTIME$@" >> $FILENAME
-		fi
+		WRITE "<ERROR> $CURRTIME$@"
 	fi
 }
 function LOGCRIT { 
 	if [ "$LOGLEVEL" -gt 1 ]
 	then
 		if [ -n "$ADDTIME" ];then CURRTIME=$(date +"%H:%M:%S ");else CURRTIME=""; fi
-		if [ -n "$NOFILE" ];then
-			echo "<CRITICAL> $CURRTIME$@"
-		elif [ -n "$STDERR" ] || [ -z ${FILENAME+x} ];then
-			echo "<CRITICAL> $CURRTIME$@" 1>&2
-		else
-			echo "<CRITICAL> $CURRTIME$@" >> $FILENAME
-		fi
+		WRITE "<CRITICAL> $CURRTIME$@"
 	fi
 }
 function LOGALERT { 
 	if [ "$LOGLEVEL" -gt 0 ]
 	then
 		if [ -n "$ADDTIME" ];then CURRTIME=$(date +"%H:%M:%S ");else CURRTIME=""; fi
-		if [ -n "$NOFILE" ];then
-			echo "<ALERT> $CURRTIME$@"
-		elif [ -n "$STDERR" ] || [ -z ${FILENAME+x} ];then
-			echo "<ALERT> $CURRTIME$@" 1>&2
-		else
-			echo "<ALERT> $CURRTIME$@" >> $FILENAME
-		fi
+		WRITE "<ALERT> $CURRTIME$@"
 	fi
 }
 function LOGEMERGE { 
 	if [ "$LOGLEVEL" -ge 0 ]
 	then
 		if [ -n "$ADDTIME" ];then CURRTIME=$(date +"%H:%M:%S ");else CURRTIME=""; fi
-		if [ -n "$NOFILE" ];then
-			echo "<EMERGE> $CURRTIME$@"
-		elif [ -n "$STDERR" ] || [ -z ${FILENAME+x} ];then
-			echo "<EMERGE> $CURRTIME$@" 1>&2
-		else
-			echo "<EMERGE> $CURRTIME$@" >> $FILENAME
-		fi
+		WRITE "<EMERGE> $CURRTIME$@"
 	fi
 }
 function LOGSTART {
-	if [ -n "$PACKAGE" ] && [ -n "$NAME" ] && ([ -n "$FILENAME" ] || [ -n "$NOFILE" ] || [-n "$STDERR" ])
+	if [ -n "$PACKAGE" ] && [ -n "$NAME" ] && ([ -n "$FILENAME" ] || [ -n "$NOFILE" ])
 	then
-		if [ -n "$STDERR" ];then
-			PARAM=--stderr
-		elif [ -n "$NOFILE" ];then
-			PARAM=--nofile
-		fi
-		LOG=(`$LBHOMEDIR/libs/bashlib/initlog.pl --name=$NAME --package=$PACKAGE --filename=$FILENAME $PARAM --message=$@`)
+		if [ -n "$STDERR" ];then PARAM=" --stderr"; fi
+		#if [ -n "$NOFILE" ];then PARAM="$PARAM --nofile";else PARAM="$PARAM --filename=$FILENAME"; fi
+		LOG=(`$LBHOMEDIR/libs/bashlib/initlog.pl --name=$NAME --package=$PACKAGE$PARAM --message=$@`)
 		FILENAME=${LOG[0]//\"}
 		if [ -z ${LOGLEVEL+x} ];then LOGLEVEL=${LOG[1]}; fi
 		if [ -z ${FILENAME+x} ] || [ -z ${LOGLEVEL+x} ]
@@ -127,15 +77,12 @@ function LOGSTART {
 function LOGEND {
 	if [ "$LOGLEVEL" -ge -1 ]
 	then
-		if [ -n "$NOFILE" ];then
-			echo "<LOGEND>$@"
-			echo "<LOGEND>"$(date +"%d.%m.%Y %H:%M:%S")" TASK FINISHED"
-		elif [ -n "$STDERR" ] || [ -z ${FILENAME+x} ];then
-			echo "<LOGEND>$@" 1>&2
-			echo "<LOGEND>"$(date +"%d.%m.%Y %H:%M:%S")" TASK FINISHED" 1>&2
-		else
-			echo "<LOGEND>$@" >> $FILENAME
-			echo "<LOGEND>"$(date +"%d.%m.%Y %H:%M:%S")" TASK FINISHED" >> $FILENAME
-		fi
+			WRITE "<LOGEND>$@"
+			WRITE "<LOGEND>"$(date +"%d.%m.%Y %H:%M:%S")" TASK FINISHED"
 	fi
 }
+function WRITE {
+		if [ -n "$NOFILE" ];then echo "$@"; fi
+		if [ -n "$STDERR" ] || ([ -z ${NOFILE+x} ] && [ -z ${FILENAME+x} ]);then echo "$@" 1>&2; fi
+		if [ -z ${NOFILE+x} ] && [ -n "$FILENAME" ];then echo "$@" >> $FILENAME; fi
+}	
