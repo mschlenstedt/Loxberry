f9ffb7f188f9ea76adc7013cacfeb929379e657b
Christian Fenzl <fenzl@t-r-t.at>
2018-01-29 09:01:44 +0100

    Testing functions for lock and unlock

--- libs/perllib/LoxBerry/testing/testlock.pl
+++ libs/perllib/LoxBerry/testing/testlock.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 use LoxBerry::System;
-use LoxBerry::Lock;
+# use LoxBerry::Lock;
 
 
 # my $lockstatus = LoxBerry::System::lock(lockfile => 'test', wait => 20);
@@ -22,20 +22,20 @@ use LoxBerry::Lock;
 
 print "Locking lbupdate...\n";
 undef $lockstatus;
-$lockstatus = LoxBerry::Lock::lock(lockfile => 'lbupdate');
+$lockstatus = LoxBerry::System::lock(lockfile => 'lbupdate');
 if ($lockstatus) {
 	print "Could not lock - locked by $lockstatus\n";
 } else {
 	print "lbupdate is locked by me\n";
 }
 sleep 60;
-
+LoxBerry::System::unlock(lockfile => 'lbupdate');
 exit;
 
 
 
 print "Now lock a test file... - this should fail\n";
-$lockstatus = LoxBerry::Lock::lock(lockfile => 'test', wait => 5);
+$lockstatus = LoxBerry::System::lock(lockfile => 'test', wait => 5);
 if ($lockstatus) {
 	print "Could not lock - locked by $lockstatus\n";
 } else {
@@ -43,15 +43,15 @@ if ($lockstatus) {
 }
 
 print "Unlock lbupdate...\n";
-LoxBerry::Lock::unlock(lockfile => 'lbupdate');
+LoxBerry::System::unlock(lockfile => 'lbupdate');
 
 print "Now lock a test file again... - this should work\n";
-$lockstatus = LoxBerry::Lock::lock(lockfile => 'test', wait => 5);
+$lockstatus = LoxBerry::System::lock(lockfile => 'test', wait => 5);
 if ($lockstatus) {
 	print "Could not lock - locked by $lockstatus\n";
 } else {
 	print "test is locked by me\n";
 }
-LoxBerry::Lock::unlock(lockfile => 'test');
+LoxBerry::System::unlock(lockfile => 'test');
 
 print "We 'forget' to unlock test - auto-cleanup should do. Byebye.\n";
