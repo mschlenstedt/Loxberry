95b41160ab1fca6d4dc21c792b01a37fb1953bc0
Christian Fenzl <fenzl@t-r-t.at>
2018-01-31 09:06:40 +0100

    loxberry_log.php: Notify for PHP

---
+++ libs/phplib/testing/testnotify.php
@@ -0,0 +1,10 @@
+#!/usr/bin/env php
+<?php
+require_once "loxberry_log.php";
+LBLog::notify("Test", "Testing", "Was geht ab?");
+LBLog::notify("Test", "Testing", "Das ist ein Fehler?", True);
+
+
+
+
+?>
