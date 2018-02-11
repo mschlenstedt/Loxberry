85b378f62818172d11ab41b825b5e8cba55aa3af
Christian Fenzl <fenzl@t-r-t.at>
2018-02-11 16:46:09 +0100

    Update-Script 1.0.1: Request a reboot

--- sbin/loxberryupdate/update_v1.0.1.pl
+++ sbin/loxberryupdate/update_v1.0.1.pl
@@ -63,8 +63,8 @@ if (-e "$lbsconfigdir/is_raspberry.cfg") {
 
 
 ## If this script needs a reboot, a reboot.required file will be created or appended
-#LOGWARN "Update file $0 requests a reboot of LoxBerry. Please reboot your LoxBerry after the installation has finished.";
-#reboot_required("LoxBerry Update requests a reboot.");
+LOGWARN "Update file $0 requests a reboot of LoxBerry. Please reboot your LoxBerry after the installation has finished.";
+reboot_required("LoxBerry Update requests a reboot.");
 
 LOGOK "Update script $0 finished." if ($errors == 0);
 LOGERR "Update script $0 finished with errors." if ($errors != 0);
