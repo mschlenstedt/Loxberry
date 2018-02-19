e7ed2a9c6818c3f16e1026e239e30f4c349d2fea
Christian Fenzl <fenzl@t-r-t.at>
2018-02-19 11:34:16 +0100

    Notifications for PHP: Introduced get_notifications
    loxberry_log.php:
    - Introduced get_notifications
    - Added example/test to testing/testnotify.php

--- libs/phplib/loxberry_log.php
+++ libs/phplib/loxberry_log.php
@@ -5,14 +5,46 @@ require_once "loxberry_system.php";
 
 class LBLog
 {
-	public static $VERSION = "1.0.0.2";
+	public static $VERSION = "1.0.0.3";
 	
-	function get_notifications ($package = NULL, $name = NULL, $latest = NULL, $count = NULL, $getcontent = NULL)
+	function get_notifications ($package = NULL, $name = NULL)
 	{
-		error_log("get_notifications called.\n");
+		// error_log("get_notifications called.\n");
 		
+		global $lbpplugindir;
+
+		$NOTIFHANDLERURL = "http://localhost:" . lbwebserverport() . "/admin/system/tools/ajax-notification-handler.cgi";	
+	
+		$fields = array(
+			'action' => 'get_notifications',
+		);
+
+		if (isset($package)) { $fields['package'] = $package; }
+		if (isset($name)) { $fields['name'] = $name; }
 		
+		$options = array(
+			'http' => array(
+				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+				'method'  => 'POST',
+				'content' => http_build_query($fields)
+				)
+		);
+		$context  = stream_context_create($options);
+		$result = file_get_contents($NOTIFHANDLERURL, false, $context);
+		if ($result === FALSE) { 
+			error_log("get_notifications_html: Could not get notifications"); 
+			return;
+		}
+		
+		$jsonresult = json_decode($result, true);
+		# var_dump($jsonresult);
+		
+		return ($jsonresult);
+	
 	}
+	
+		
+	
 	
 	// get_notifications_html
 	function get_notifications_html ($package = NULL, $name = NULL, $type = NULL, $buttons = NULL)
--- libs/phplib/testing/testnotify.php
+++ libs/phplib/testing/testnotify.php
@@ -4,7 +4,15 @@ require_once "loxberry_log.php";
 notify("Test", "Testing", "Was geht ab?");
 notify("Test", "Testing", "Das ist ein Fehler?", True);
 
-
+$notifications = LBLog::get_notifications("Test", "Testing");
+foreach($notifications as $notification) {
+		if (isset($notification['_ISPLUGIN'])) {
+			print "This is a plugin notification from " . $notification['PACKAGE'] . ":\n"; 
+		} elseif (isset($notification['_ISSYSTEM'])) {
+			print "This is a system notification from " . $notification['PACKAGE'] . ":\n"; 
+		}
+		echo "Name: {$notification['NAME']} Message: {$notification['CONTENTRAW']} Severity {$notification['SEVERITY']}\n";
+}
 
 
 ?>
