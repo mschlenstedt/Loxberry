33982ef49a0d3ccdfd1c009954b2592211ea127c
Christian Fenzl <fenzl@t-r-t.at>
2018-02-26 14:30:01 +0100

    Fixed problems with 1.0.3 update script

--- sbin/loxberryupdate/update_v1.0.3.pl
+++ sbin/loxberryupdate/update_v1.0.3.pl
@@ -41,6 +41,10 @@ my $cgi = CGI->new;
 my $errors = 0;
 LOGOK "Update script $0 started.";
 
+#
+# Notify
+#
+
 LOGOK "This update migrates the notification feature from file-based to SQLite-based and introduces new notification functions. Therefore, old notifications will be deleted.";
 delete_directory ("$lbsdatadir/notifications");
 
@@ -49,6 +53,9 @@ if (-e "$lbsdatadir/notifications_sqlite.dat" ) {
 	unlink "$lbsdatadir/notifications_sqlite.dat";
 }
 
+#
+# Sudoers
+#
  
 #LOGINF "Replacing system default sudoers file";
 #LOGINF "Copying new";
@@ -64,13 +71,18 @@ if (-e "$lbsdatadir/notifications_sqlite.dat" ) {
 #}
 #qx { chown root:root $lbhomedir/system/sudoers/lbdefaults };
 
+#
+# usbmount
+#
+
 LOGINF "Installing packages usbmount and ntfs-3g";
 
 my $output = qx { /usr/bin/dpkg --configure -a };
 my $exitcode  = $? >> 8;
 if ($exitcode != 0) {
         LOGERR "Error configuring dkpg with /usr/bin/dpkg --configure -a - Error $exitcode";
-        $errors++;
+        LOGDEB $output;
+		$errors++;
 } else {
         LOGOK "Configuring dpkg successfully.";
 }
@@ -78,6 +90,7 @@ $output = qx { /usr/bin/apt-get -q -y update };
 $exitcode  = $? >> 8;
 if ($exitcode != 0) {
         LOGERR "Error updating apt database - Error $exitcode";
+		LOGDEB $output;
         $errors++;
 } else {
         LOGOK "Apt database updated successfully.";
@@ -86,51 +99,41 @@ $output = qx { /usr/bin/apt-get -q -y install usbmount ntfs-3g };
 $exitcode  = $? >> 8;
 if ($exitcode != 0) {
 	LOGERR "Error installing packages usbmount ntfs-3g with apt-get - Error $exitcode";
+	LOGDEB $output;
 	$errors++;
 } else {
 	LOGOK "Installing usbmount ntfs-3g successfully.";
 }
 
 if ( -e "/etc/usbmount/usbmount.conf" ) {
-	$output = qx {awk -v s='FILESYSTEMS="vfat ntfs fuseblk ext2 ext3 ext4 hfsplus"' '/^FILESYSTEMS=/{$0=s;f=1} {a[++n]=$0} END{if(!f)a[++n]=s;for(i=1;i<=n;i++)print a[i]>ARGV[1]}' /etc/usbmount/usbmount.conf };
+	$output = qx {awk -v s='FILESYSTEMS="vfat ntfs fuseblk ext2 ext3 ext4 hfsplus"' '/^FILESYSTEMS=/{\$0=s;f=1} {a[++n]=\$0} END{if(!f)a[++n]=s;for(i=1;i<=n;i++)print a[i]>ARGV[1]}' /etc/usbmount/usbmount.conf };
 	$exitcode  = $? >> 8;
 	if ($exitcode != 0) {
        		LOGERR "Error replacing string FILESYSTEMS= in /etc/usbmount/usbmount.conf - Error $exitcode";
+			LOGDEB $output;
       		$errors++;
 	} else {
         	LOGOK "Replacing string FILESYSTEMS= successfully in /etc/usbmount/usbmount.conf.";
 	}
-	$output = qx {awk -v s='FS_MOUNTOPTIONS="-fstype=ntfs-3g,nls=utf8,umask=007,gid=1001 -fstype=fuseblk,nls=utf8,umask=007,gid=1001 -fstype=vfat,gid=1001,uid=1001,umask=007"' '/^FS_MOUNTOPTIONS=/{$0=s;f=1} {a[++n]=$0} END{if(!f)a[++n]=s;for(i=1;i<=n;i++)print a[i]>ARGV[1]}' /etc/usbmount/usbmount.conf };
+	$output = qx {awk -v s='FS_MOUNTOPTIONS="-fstype=ntfs-3g,nls=utf8,umask=007,gid=1001 -fstype=fuseblk,nls=utf8,umask=007,gid=1001 -fstype=vfat,gid=1001,uid=1001,umask=007"' '/^FS_MOUNTOPTIONS=/{\$0=s;f=1} {a[++n]=\$0} END{if(!f)a[++n]=s;for(i=1;i<=n;i++)print a[i]>ARGV[1]}' /etc/usbmount/usbmount.conf };
 	$exitcode  = $? >> 8;
 	if ($exitcode != 0) {
        		LOGERR "Error replacing string FS_MOUNTOPTIONS= in /etc/usbmount/usbmount.conf - Error $exitcode";
+			LOGDEB $output;
       		$errors++;
 	} else {
         	LOGOK "Replacing string FS_MOUNTOPTIONS= successfully in /etc/usbmount/usbmount.conf.";
 	}
 }
-$output = qx { ln -s /var/run/usbmount/ $lbhomedir/system/storage };
+$output = qx { ln -f -s /var/run/usbmount/ $lbhomedir/system/storage };
 $exitcode  = $? >> 8;
 if ($exitcode != 0) {
 	LOGERR "Error creating symlink $lbhomedir/system/storage - Error $exitcode";
+	LOGDEB $output;
 	$errors++;
 } else {
 	LOGOK "Symlink $lbhomedir/system/storage created successfully";
 }
-
-LOGINF "Replacing system samba config file";
-LOGINF "Copying new";
-
-$output = qx { if [ -e $updatedir/system/samba/smb.conf ] ; then cp -f $updatedir/system/samba/smb.conf $lbhomedir/system/samba/ ; fi };
-$exitcode  = $? >> 8;
-
-if ($exitcode != 0) {
-	LOGERR "Error copying new samba config file - Error $exitcode";
-	$errors++;
-} else {
-	LOGOK "New samba config file copied.";
-}
-qx { chown loxberry:loxberry $lbhomedir/system/samba/smb.conf };
 
 LOGINF "Creating /etc/systemd/system/systemd-udevd.service";
 
@@ -170,6 +173,45 @@ RestrictAddressFamilies=AF_UNIX AF_NETLINK AF_INET AF_INET6
 EOF
 	close (F);
 }
+
+#
+# Samba
+#
+
+LOGINF "Replacing system samba config file";
+LOGINF "Copying new";
+
+$output = qx { if [ -e $updatedir/system/samba/smb.conf ] ; then cp -f $updatedir/system/samba/smb.conf $lbhomedir/system/samba/ ; fi };
+$exitcode  = $? >> 8;
+
+if ($exitcode != 0) {
+	LOGERR "Error copying new samba config file - Error $exitcode";
+	LOGDEB $output;
+      			$errors++;
+} else {
+	LOGOK "New samba config file copied.";
+}
+
+qx { chown loxberry:loxberry $lbhomedir/system/samba/smb.conf };
+
+#
+# i2c
+#
+
+LOGINF "Activating i2c";
+
+$output = qx { $lbhomedir/sbin/activate_i2c.sh };
+$exitcode  = $? >> 8;
+
+if ($exitcode != 0) {
+	LOGERR "Error activating i2c - Error $exitcode";
+	LOGDEB $output;
+	$errors++;
+} else {
+	LOGOK "i2c activated.";
+}
+
+
 
 ## If this script needs a reboot, a reboot.required file will be created or appended
 LOGWARN "Update file $0 requests a reboot of LoxBerry. Please reboot your LoxBerry after the installation has finished.";
