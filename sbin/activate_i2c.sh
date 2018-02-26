2ed3d4d61b4523a5afc30082d35496406dc14f92
Christian Fenzl <fenzl@t-r-t.at>
2018-02-26 11:28:10 +0100

    activate_i2c.sh: Fixed invalid apt-get
    - Fixed apt-get install update (is now apt-get update)

--- sbin/activate_i2c.sh
+++ sbin/activate_i2c.sh
@@ -4,5 +4,5 @@ awk -v s="" '/^[#]?dtparam=i2c1=/{$0=s;f=1} {a[++n]=$0} END{if(!f)a[++n]=s;for(i
 awk -v s="dtparam=i2c1=on\ndtparam=i2c_arm=on" '/^[#]?dtparam=i2c_arm=/{$0=s;f=1} {a[++n]=$0} END{if(!f)a[++n]=s;for(i=1;i<=n;i++)print a[i]>ARGV[1]}' /boot/config.txt
 awk -v s="i2c-bcm2708" '/^[#]?i2c-bcm2708/{$0=s;f=1} {a[++n]=$0} END{if(!f)a[++n]=s;for(i=1;i<=n;i++)print a[i]>ARGV[1]}' /etc/modules
 awk -v s="i2c-dev" '/^[#]?i2c-dev/{$0=s;f=1} {a[++n]=$0} END{if(!f)a[++n]=s;for(i=1;i<=n;i++)print a[i]>ARGV[1]}' /etc/modules
-apt-get install update
+apt-get update
 apt-get install i2c-tools
