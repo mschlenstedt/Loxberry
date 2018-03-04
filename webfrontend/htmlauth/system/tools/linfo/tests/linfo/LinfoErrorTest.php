f0e11b699e3405004151c965370d65dc5d9241cc
mschlenstedt <michael@schlenn.net>
2018-03-04 20:56:22 +0100

    Linfo: Maybe for future tab in MyLoxBerry

---
+++ webfrontend/htmlauth/system/tools/linfo/tests/linfo/LinfoErrorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use \Linfo\Meta\Errors;
+
+class ErrorTest extends PHPUnit_Framework_TestCase
+{
+
+  /**
+   * @test
+   */
+  public function add()
+  {
+      Errors::add('testing', 'testing 123');
+      Errors::add('testing', 'testing 456');
+      $this->assertCount(2, Errors::show());
+  }
+
+    public function tearDown()
+    {
+        Errors::clear();
+    }
+}
---
+++ webfrontend/htmlauth/system/tools/linfo/tests/test_lang.php
@@ -0,0 +1,3 @@
+<?php
+
+return require dirname(dirname(__FILE__)).'/src/Linfo/Lang/en.php';
