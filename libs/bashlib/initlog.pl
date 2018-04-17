da960592377a4a6a272828fdce6e78f23b64c531
Christian Fenzl <fenzl@t-r-t.at>
2018-04-17 18:35:44 +0200

    Updated initlog.pl to support new and startlog as different calls

--- libs/bashlib/initlog.pl
+++ libs/bashlib/initlog.pl
@@ -5,6 +5,7 @@ use Getopt::Long;
 
 # Collecting options
 GetOptions (
+	'action' => \$action,
 	'package=s' => \$package,
 	'name=s' => \$name,
 	'filename=s' => \$logfilename,
@@ -12,23 +13,60 @@ GetOptions (
 	'append' => \$append,
 	'nofile' => \$nofile,
 	'stderr' => \$stderr,
+	'stdout' => \$stdout,
 	'message=s' => \$message,
 	'loglevel=s' => \$loglevel,
 );
 
-my $log = LoxBerry::Log->new ( 
-	package => $package, 
-	name => $name,
-	filename => $logfilename,
-	logdir => $logdir,
-	append => $append,
-	nofile => $nofile,
-	stderr => $stderr,
-);
+if (!$action) { 
+	init();
+	start();
+	exit(0);
+}
 
-LOGSTART $message;
-my $currfilename = $log->close;
-my $currloglevel = $log->loglevel;
-print "\"$currfilename\" $currloglevel\n";
+$action = lc($action);
+
+if( $action eq "init" ) {
+	init();
+	my $currfilename = $log->close;
+	my $currloglevel = $log->loglevel;
+	print "\"$currfilename\" $currloglevel\n";
+	exit(0);
+} elsif ( $action eq "start" ) {
+	start();
+	exit(0);
+} elsif ( $action eq "end" ) {
+	start();
+	exit(0);
+}
 
 exit;
+
+sub init
+{
+
+	my $log = LoxBerry::Log->new ( 
+		package => $package, 
+		name => $name,
+		filename => $logfilename,
+		logdir => $logdir,
+		append => $append,
+		nofile => $nofile,
+		stderr => $stderr,
+		stdout => $stdout,
+	);
+	my $currfilename = $log->close;
+	my $currloglevel = $log->loglevel;
+	print "\"$currfilename\" $currloglevel\n";
+	}
+
+sub start
+{
+	LOGSTART $message;
+	
+}
+
+sub end
+{
+	LOGEND $message;
+}
