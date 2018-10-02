21345e0040199344f9982d343d2beefba71c3374
Michael Schlenstedt <Michael@schlenn.net>
2018-10-02 22:38:01 +0200

    Optimized log cleanup

--- sbin/log_maint.pl
+++ sbin/log_maint.pl
@@ -7,11 +7,16 @@ use LoxBerry::Log;
 use CGI;
 use strict;
 
+# Global vars
+our $deletefactor;
+our %disks = LoxBerry::System::diskspaceinfo();
+
 my $log = LoxBerry::Log->new (
     package => 'core',
 	name => 'Log Maintenance',
 	logdir => "$lbhomedir/log/system_tmpfs",
-	loglevel => LoxBerry::System::systemloglevel(),
+	#loglevel => LoxBerry::System::systemloglevel(),
+	loglevel => 7,
 	stdout => 1
 );
 LOGSTART;
@@ -65,7 +70,6 @@ sub reduce_logfiles
 	LOGTITLE "reduce_logfiles";
 	LOGINF "Logfile maintenance: reduce_logfiles called.";
 	logfiles_cleanup();
-	my @logs = LoxBerry::Log::get_logs();
 	# Vacuum logdb 
 	if (!-e "$lbhomedir/log/system_tmpfs/logs_sqlite.dat" && -e "$lbhomedir/log/system/logs_sqlite.dat.bkp") {
 		qx { cp -f $lbhomedir/log/system/logs_sqlite.dat.bkp $lbhomedir/log/system_tmpfs/logs_sqlite.dat };
@@ -82,64 +86,249 @@ sub reduce_logfiles
 
 sub logfiles_cleanup
 {
-	# If less than this percent is free, start housekeeping
-	our $deletefactor = 25;
-	
-	my %disks = LoxBerry::System::diskspaceinfo();
-	foreach my $disk (keys %disks) {
-		LOGDEB "Checking $disks{$disk}{mountpoint} ($disks{$disk}{filesystem} - Available " . $disks{$disk}{available}/$disks{$disk}{size}*100;
-		next if($disks{$disk}{filesystem} ne "tmpfs");
-		next if( $disks{$disk}{size} eq "0" or ($disks{$disk}{available}/$disks{$disk}{size}*100) > $deletefactor );
-		LOGDEB "--> $disks{$disk}{mountpoint} below limit AVAL $disks{$disk}{available} SIZE $disks{$disk}{size} - housekeeping...";
-		
-		our $diskavailable = $disks{$disk}{available};
-		our $disksize = $disks{$disk}{size};
-		
-		require File::Find;
-		File::Find::find ( { preprocess => \&logfiles_orderbydate, wanted => \&logfiles_delete }, $disks{$disk}{mountpoint} );
-	
-		undef $diskavailable;
-		undef $disksize;
+
+	# Housekeeping Limits
+	$deletefactor = 25; # in %
+	my $size = 1; # in MB
+	my $logdays = 30; # in days
+	my $gzdays = 60; # in days
+
+	my $logmtime = time() - (60*60*24*$logdays);
+	my $gzmtime = time() - (60*60*24*$gzdays);
+	my @paths;
+
+	# Check which disks must be cleaned
+	LOGDEB "*** STAGE 1: Scanning for tmpfs disks... ***";
+	@paths = &checkdisks();
+
+	if (!@paths) {
+		LOGDEB "No tmpfs disk available.";
+		exit(0);
 	}
+	
+	require File::Find::Rule;
+	my $bins = LoxBerry::System::get_binaries();
+
+	foreach (@paths) {
+
+		LOGDEB "Scanning $_ for LOG-Files >= $size MB and GZIP them...";
+
+		my @files = File::Find::Rule->file()
+			->name( '*.log' )
+			->size( ">=$size" . "M" )
+        		->in($_);
+
+		for my $file (@files){
+			LOGDEB "--> $file (Size is: " . sprintf("%.1f",(-s "$file")/1000/1000) . " MB) will be GZIP'd.";
+			my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks) = stat("$file");
+			unlink ("$file\.gz") if (-e "$file");
+			qx{yes | $bins->\{GZIP\} --best "$file"};
+			chown $uid, $gid, "$file\.gz";
+			chmod $mode, "$file\.gz";
+		}	
+
+		LOGDEB "Scanning $_ for LOG-Files >= " . $logdays . " days and GZIP them...";
+
+		my @files = File::Find::Rule->file()
+			->name( '*.log' )
+			->mtime( "<=$logmtime")
+        		->in($_);
+
+		for my $file (@files){
+			LOGDEB "--> $file (Age is: " . sprintf("%.1f",(-M "$file")) . " days) will be GZIP'd.";
+			my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks) = stat("$file");
+			unlink ("$file\.gz") if (-e "$file");
+			qx{yes | $bins->\{GZIP\} --best "$file"};
+			chown $uid, $gid, "$file\.gz";
+			chmod $mode, "$file\.gz";
+		}	
+
+		LOGDEB "Scanning $_ for GZ-Files >= " . $gzdays . " days and DELETE them...";
+
+		my @files = File::Find::Rule->file()
+			->name( '*.gz' )
+			->mtime( "<=$gzmtime")
+        		->in($_);
+
+		for my $file (@files){
+			LOGDEB "--> $file (Age is: " . sprintf("%.1f",(-M "$file")) . " days) will be DELETED.";
+			my $delcount = unlink ("$file");
+			if($delcount) {
+				LOGDEB "$file DELETED.";
+			} else {
+				LOGDEB "$file COULD NOT BE DELETED.";
+			}
+		}	
+
+	}
+	undef @paths;
+
+	# Re-Check which disks must still be cleaned
+	LOGDEB "*** STAGE 2: Scanning for tmpfs disks below $deletefactor% free capacity... ***";
+	@paths = &checkdisks_emerg();
+
+	if (!@paths) {
+		LOGDEB "No emergency housekeeping for any disk needed.";
+		exit(0);
+	}
+	
+	foreach (@paths) {
+		LOGDEB "Scanning $_ for GZ-Files >= " . $size . " MB and DELETE them...";
+
+		my @files = File::Find::Rule->file()
+			->name( '*.gz' )
+			->size( ">=$size" . "M" )
+        		->in($_);
+
+		for my $file (@files){
+			LOGDEB "--> $file (Size is: " . sprintf("%.1f",(-s "$file")/1000/1000) . " MB) will be DELETED.";
+			my $delcount = unlink ("$file");
+			if($delcount) {
+				LOGDEB "$file DELETED.";
+			} else {
+				LOGDEB "$file COULD NOT BE DELETED.";
+			}
+		}	
+	}
+	undef @paths;
+	
+	# Re-Check which disks must still be cleaned
+	LOGDEB "*** STAGE 3: Re-Scanning for tmpfs disks below $deletefactor% free capacity... ***";
+	@paths = &checkdisks_emerg();
+
+	if (!@paths) {
+		LOGDEB "No emergency housekeeping for any disk needed.";
+		exit(0);
+	}
+	
+	foreach (@paths) {
+		LOGDEB "Scanning $_ for any GZ-Files and DELETE them...";
+
+		my @files = File::Find::Rule->file()
+			->name( '*.gz' )
+        		->in($_);
+
+		for my $file (@files){
+			LOGDEB "--> $file will be DELETED.";
+			my $delcount = unlink ("$file");
+			if($delcount) {
+				LOGDEB "$file DELETED.";
+			} else {
+				LOGDEB "$file COULD NOT BE DELETED.";
+			}
+		}	
+	}
+	undef @paths;
+	
+	# Re-Check which disks must still be cleaned
+	LOGDEB "*** STAGE 4: Re-Scanning for tmpfs disks below $deletefactor% free capacity... ***";
+	@paths = &checkdisks_emerg();
+
+	if (!@paths) {
+		LOGDEB "No emergency housekeeping for any disk needed.";
+		exit(0);
+	}
+	
+	foreach (@paths) {
+		LOGDEB "Scanning $_ for any LOG-Files and DELETE them...";
+
+		my @files = File::Find::Rule->file()
+			->name( '*.log' )
+        		->in($_);
+
+		for my $file (@files){
+			LOGDEB "--> $file will be DELETED.";
+			my $delcount = unlink ("$file");
+			if($delcount) {
+				LOGDEB "$file DELETED.";
+			} else {
+				LOGDEB "$file COULD NOT BE DELETED.";
+			}
+		}	
+	}
+	
+	# If less than this percent is free, start housekeeping
+	#our $deletefactor = 25;
+	
+	#my %disks = LoxBerry::System::diskspaceinfo();
+	#foreach my $disk (keys %disks) {
+	#	LOGDEB "Checking $disks{$disk}{mountpoint} ($disks{$disk}{filesystem} - Available " . $disks{$disk}{available}/$disks{$disk}{size}*100;
+	#	next if($disks{$disk}{filesystem} ne "tmpfs");
+	#	next if( $disks{$disk}{size} eq "0" or ($disks{$disk}{available}/$disks{$disk}{size}*100) > $deletefactor );
+	#	LOGDEB "--> $disks{$disk}{mountpoint} below limit AVAL $disks{$disk}{available} SIZE $disks{$disk}{size} - housekeeping...";
+	#	
+	#	our $diskavailable = $disks{$disk}{available};
+	#	our $disksize = $disks{$disk}{size};
+	#	
+	#	require File::Find;
+	#	File::Find::find ( { preprocess => \&logfiles_orderbydate, wanted => \&logfiles_delete }, $disks{$disk}{mountpoint} );
+	#
+	#	undef $diskavailable;
+	#	undef $disksize;
+	#}
 }	
 
-sub logfiles_orderbydate
-{
-	my @files = @_;
-	my @filesnew;
-	
-	LOGDEB "logfiles_orderbydate called for folder $File::Find::dir";
-	
-	foreach my $filename (@files) {
-		next if ($filename eq ".");
-		next if ($filename eq "..");
-		push(@filesnew, $filename);
+sub checkdisks {
+	my @paths;
+	foreach my $disk (keys %disks) {
+		LOGDEB "Checking $disks{$disk}{mountpoint} ($disks{$disk}{filesystem})";
+		next if($disks{$disk}{filesystem} ne "tmpfs");
+		next if( $disks{$disk}{size} eq "0" );
+		LOGDEB "--> $disks{$disk}{mountpoint} - housekeeping needed.";
+		push(@paths, $disks{$disk}{mountpoint});
 	}
-	
-	@filesnew = sort {(stat $a)[10] <=> (stat $b)[10]} @filesnew;
-
-	return @filesnew;
+	return(@paths);
 }
 
-sub logfiles_delete
-{
-	
-	return if (-d $File::Find::name);
-	return if (index($_, ".log") == -1);
-	return if (! $LoxBerry::Log::deletefactor or ($LoxBerry::Log::diskavailable / $LoxBerry::Log::disksize * 100) > ($LoxBerry::Log::deletefactor+3));
-	my $size = (stat $File::Find::name)[7] / 1024;
-	LOGDEB "logfiles_delete called with $File::Find::name (SIZE $size KB, Available: $LoxBerry::Log::diskavailable KB)";
-	# Unlink
-	my $delcount = unlink $File::Find::name;
-	if($delcount) {
-		LOGDEB "   DELETED $_";
-		$LoxBerry::Log::diskavailable += $size;
-	} else {
-		LOGDEB "   COULD NOT DELETE $_";
+sub checkdisks_emerg {
+	my @paths;
+	foreach my $disk (keys %disks) {
+		LOGDEB "Checking $disks{$disk}{mountpoint} ($disks{$disk}{filesystem} - Available " . sprintf("%.1f",$disks{$disk}{available}/$disks{$disk}{size}*100) . "%)";
+		next if($disks{$disk}{filesystem} ne "tmpfs");
+		next if( $disks{$disk}{size} eq "0" or ($disks{$disk}{available}/$disks{$disk}{size}*100) > $deletefactor );
+		LOGDEB "--> $disks{$disk}{mountpoint} below limit AVAL $disks{$disk}{available} SIZE $disks{$disk}{size} - EMERGENCY housekeeping needed.";
+		push(@paths, $disks{$disk}{mountpoint});
 	}
-	return;
-
+	return(@paths);
 }
+
+#sub logfiles_orderbydate
+#{
+#	my @files = @_;
+#	my @filesnew;
+#	
+#	LOGDEB "logfiles_orderbydate called for folder $File::Find::dir";
+#	
+#	foreach my $filename (@files) {
+#		next if ($filename eq ".");
+#		next if ($filename eq "..");
+#		push(@filesnew, $filename);
+#	}
+#	
+#	@filesnew = sort {(stat $a)[10] <=> (stat $b)[10]} @filesnew;
+#
+#	return @filesnew;
+#}
+
+#sub logfiles_delete
+#{
+#	
+#	return if (-d $File::Find::name);
+#	return if (index($_, ".log") == -1);
+#	return if (! $LoxBerry::Log::deletefactor or ($LoxBerry::Log::diskavailable / $LoxBerry::Log::disksize * 100) > ($LoxBerry::Log::deletefactor+3));
+#	my $size = (stat $File::Find::name)[7] / 1024;
+#	LOGDEB "logfiles_delete called with $File::Find::name (SIZE $size KB, Available: $LoxBerry::Log::diskavailable KB)";
+#	# Unlink
+#	my $delcount = unlink $File::Find::name;
+#	if($delcount) {
+#		LOGDEB "   DELETED $_";
+#		$LoxBerry::Log::diskavailable += $size;
+#	} else {
+#		LOGDEB "   COULD NOT DELETE $_";
+#	}
+#	return;
+#
+#}
 
 #############################################################
 # Function backup_logdb (every week)
