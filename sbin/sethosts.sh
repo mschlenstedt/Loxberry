0c75785bcaad80f792f0c98e03dfb966326270dd
Svethi <svethi@loxberry.de>
2018-10-29 20:20:50 +0100

    add file for fixing DNS resolv problems

---
+++ sbin/sethosts.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# ifup hook script for resolvconf
+# Written by Roy Marples <roy@marples.name> under the BSD-2 license
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+
+HN=$(hostname)
+IF=$(route -n | awk '/UG/ {print $8}')
+IP=$(ip -4 -o addr show dev $IF |awk '{split($4,a,"/") ;print a[1]}')
+
+sed -i /$HN/d /etc/hosts
+echo "$IP\t$HN" >> /etc/hosts
+systemctl restart dnsmasq 2>&1 > /dev/null
