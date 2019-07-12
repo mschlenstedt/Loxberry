44331c175c4b11e371bb3cda8c8af7713570daa3
Michael Schlenstedt <Michael@schlenn.net>
2019-07-12 06:06:56 +0200

    Logfile of current update

--- sbin/loxberryupdatewebserver.pl
+++ sbin/loxberryupdatewebserver.pl
@@ -7,9 +7,13 @@ my $d = HTTP::Daemon->new(
 	LocalPort => 80,
 	) || die;
 
+my $logfile = shift;
+
 while (my $c = $d->accept) {
     while (my $r = $c->get_request) {
-        if ($r->method eq 'GET') {
+        if ($r->method eq 'GET' && $r->uri->path eq "/logfile") {
+            $c->send_file_response("$lbslogdir/loxberryupdate/$logfile");
+        } else {
             $c->send_file_response("$lbstemplatedir/reboot_updaterunning.html");
         }
     }
