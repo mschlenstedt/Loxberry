3fba6ec906e4945100e757df90c95eb4b321d0dc
Michael Schlenstedt <Michael@schlenn.net>
2019-07-14 17:10:46 +0200

    Use Apache Port; Power-handler implemented

--- sbin/loxberryupdate/updaterebootwebserver.pl
+++ sbin/loxberryupdate/updaterebootwebserver.pl
@@ -3,8 +3,10 @@ use HTTP::Daemon;
 use HTTP::Status;
 use LoxBerry::System;
  
+my $port = lbwebserverport();
+
 my $d = HTTP::Daemon->new(
-	LocalPort => 80,
+	LocalPort => $port,
 	) || die;
 
 my $logfile = shift;
@@ -13,6 +15,8 @@ while (my $c = $d->accept) {
     while (my $r = $c->get_request) {
         if ($r->method eq 'GET' && $r->uri->path eq "/logfile") {
             $c->send_file_response("$logfile");
+    	} elsif ($r->method eq 'GET' && $r->uri->path eq "/admin/system/tools/power-handler.php") {
+		$c->send_file_response("$lbstemplatedir/reboot_updaterunning_ph.html");
         } else {
             $c->send_file_response("$lbstemplatedir/reboot_updaterunning.html");
         }
