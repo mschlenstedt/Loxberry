f4796a364088ca5275386ddf9bc043faa5040b8b
Christian Fenzl <fenzl@t-r-t.at>
2019-07-19 19:04:19 +0200

    updaterebootwebserver: Some visual improvements

--- sbin/loxberryupdate/updaterebootwebserver.pl
+++ sbin/loxberryupdate/updaterebootwebserver.pl
@@ -4,6 +4,7 @@ use HTTP::Status;
 use LoxBerry::System;
  
 my $port = lbwebserverport();
+# my $port = 80;
 
 my $d = HTTP::Daemon->new(
 	LocalPort => $port,
@@ -15,7 +16,9 @@ while (my $c = $d->accept) {
     while (my $r = $c->get_request) {
         if ($r->method eq 'GET' && $r->uri->path eq "/logfile") {
             $c->send_file_response("$logfile");
-    	} elsif ($r->method eq 'GET' && $r->uri->path eq "/admin/system/tools/power-handler.php") {
+    	} elsif ($r->method eq 'GET' && $r->uri->path eq "/updatereboot-anim-large.gif") {
+			$c->send_file_response("$lbshtmldir/images/updatereboot-anim-large.gif");
+		} elsif ($r->method eq 'GET' && $r->uri->path eq "/admin/system/tools/power-handler.php") {
 		$c->send_file_response("$lbstemplatedir/reboot_updaterunning_ph.html");
         } else {
             $c->send_file_response("$lbstemplatedir/reboot_updaterunning.html");
