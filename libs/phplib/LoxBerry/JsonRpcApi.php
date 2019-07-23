7ed6f73f1640f6c6f0508ce0926aa279dd1f4002
Christian Fenzl <fenzl@t-r-t.at>
2019-07-23 15:10:37 +0200

    JsonRpc: Prevent lowlevel system functions

--- libs/phplib/LoxBerry/JsonRpcApi.php
+++ libs/phplib/LoxBerry/JsonRpcApi.php
@@ -2,8 +2,41 @@
 
 namespace LoxBerry;
 
-// Version 1.5.0.1
+// Version 1.5.0.2
 
+// These functions are prevented by the server for security reasons
+CONST PREVENTED_FUNCTIONS = [ 
+	"apache_child_terminate", 
+	"assert", 
+	"create_function", 
+	"eval", 
+	"exec", 
+	"fsockopen", 
+	"include", 
+	"include_once", 
+	"ini_set", 
+	"passthru", 
+	"pcntl_exec", 
+	"phpinfo", 
+	"pfsockopen", 
+	"popen", 
+	"posix_kill", 
+	"posix_mkfifo", 
+	"posix_setpgid", 
+	"posix_setsid", 
+	"posix_setuid",
+	"preg_replace", 
+	"proc_close", 
+	"proc_nice", 
+	"proc_open", 
+	"proc_terminate", 
+	"putenv", 
+	"require", 
+	"require_once", 
+	"shell_exec", 
+	"system", 
+	"use", 
+];
 
 require_once "/opt/loxberry/libs/phplib/loxberry_system.php";
 // require_once "/opt/loxberry/libs/phplib/loxberry_web.php";
@@ -24,17 +57,17 @@ class JsonRpcApi implements Evaluator
         }
 	*/
 		
-		echo "Method: $method\n";
-		echo "Arguments: " . join(" ", $arguments) . "\n";
-		list ($ns, $function) = explode("::", $method, 2);
+		//echo "Method: $method\n";
+		//echo "Arguments: " . join(" ", $arguments) . "\n";
+		
+		@list ($ns, $function) = explode("::", $method, 2);
 		if(empty($function)) {
 			$function = $ns;
 			$ns = null;
 		}
 		
-		echo "Namespace: $ns\n";
-		echo "Function : $function\n";
-		
+		// echo "Namespace: $ns\n";
+		// echo "Function : $function\n";
 		
 		$func_exists=false;
 		if(empty($ns) && function_exists($function)) {
@@ -43,10 +76,13 @@ class JsonRpcApi implements Evaluator
 			$func_exists=true;
 		}
 		
+		// Prevent non-allowed functions
+		if( in_array( strtolower($function), PREVENTED_FUNCTIONS ) ) {
+			error_log("JsonRPC: $function is a prevented function.");
+			$func_exists=false;
+		}
+		
 		if($func_exists) {
-			// return call_user_func($method, $arguments);
-			// return $method($arguments);
-			// return $method("squeezelite");
 			return call_user_func_array($method, $arguments);
 		}
 		
@@ -54,14 +90,14 @@ class JsonRpcApi implements Evaluator
 		
 	}
 
-    private static function add($arguments)
-    {
-        @list($a, $b) = $arguments;
+    // private static function add($arguments)
+    // {
+        // @list($a, $b) = $arguments;
 
-        if (!is_int($a) || !is_int($b)) {
-            throw new ArgumentException();
-        }
+        // if (!is_int($a) || !is_int($b)) {
+            // throw new ArgumentException();
+        // }
 
-        return "10";
-    }
+        // return "10";
+    // }
 }
--- webfrontend/htmlauth/system/jsonrpc.php
+++ webfrontend/htmlauth/system/jsonrpc.php
@@ -1,28 +1,29 @@
 <?php
 
-class Autoloader
-{
-    public static function register()
-    {
-        spl_autoload_register(function ($class) {
-            $file = str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php';
-            if (file_exists($file)) {
-                require $file;
-                return true;
-            }
-            return false;
-        });
-    }
-}
-Autoloader::register();
+require_once "loxberry_system.php";
+
+// autoload classes based on a 1:1 mapping from namespace to directory structure.
+spl_autoload_register(function ($className) {
+
+    # Usually I would just concatenate directly to $file variable below
+    # this is just for easy viewing on Stack Overflow)
+        $ds = DIRECTORY_SEPARATOR;
+        $dir = LBHOMEDIR . "/libs/phplib";
+
+    // replace namespace separator with directory separator (prolly not required)
+        $className = str_replace('\\', $ds, $className);
+
+    // get full name of file containing the required class
+        $file = "{$dir}{$ds}{$className}.php";
+
+    // get file if it is readable
+        if (is_readable($file)) require_once $file;
+});
 
 $args = $argv;
 array_shift($args);
 $jsonquery = join(" ", $args);
-echo "Arg: $jsonquery\n";
-
-
-
+# echo "Arg: $jsonquery\n";
 
 use Datto\JsonRpc\Evaluator;
 use Datto\JsonRpc\Exceptions\ArgumentException;
@@ -30,21 +31,8 @@ use Datto\JsonRpc\Exceptions\MethodException;
 use Datto\JsonRpc\Server;
 use LoxBerry\JsonRpcApi;
 
-$server = new Server(new Api());
+$server = new Server(new JsonRpcApi());
 
 $reply = $server->reply($jsonquery);
 
 echo $reply, "\n"; // {"jsonrpc":"2.0","id":1,"result":3}
-
-
-
-
-
-
-
-
-
-
-
-
-
