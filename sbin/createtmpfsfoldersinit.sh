e233d81a6c4e948f71d9cefe377b2ac49a394cf5
Michael Schlenstedt <Michael@schlenn.net>
2019-09-14 08:17:28 +0200

    Real systemd for createtmpfsfoldersinit

--- sbin/createtmpfsfoldersinit.sh
+++ sbin/createtmpfsfoldersinit.sh
@@ -1,20 +1,7 @@
 #!/bin/sh
-### BEGIN INIT INFO
-# Provides:          createtmpfsfolder
 # Required-Start:    $local_fs
-# Required-Stop:     
-# Default-Start:     2 3 4 5
-# Default-Stop:      
 # X-Start-Before:    cron nmbd smbd samba-ad-dc bootlogs apache2 lighttpd
 # Short-Description: create log folders on tmpfs after mountall
-# Description:       This file creates needed folders in syslog and
-#                    loxberry system log folders.
-### END INIT INFO
-
-
-. /lib/lsb/init-functions
-ENVIRONMENT=$(cat /etc/environment)
-export $ENVIRONMENT
 
 PATH="/sbin:/bin:/usr/sbin:/usr/bin:/opt/loxberry/bin:/opt/loxberry/sbin:$LBHOMEDOR/bin:$LBHOMEDIR/sbin"
 PIVERS=`$LBHOMEDIR/bin/showpitype`
@@ -110,8 +97,7 @@ wait_for () {
 
 case "$1" in
 
-start|"")
-
+start)
 	if [ ! -d $RAM_LOG ]; then
 		mkdir -p ${RAM_LOG}
 	fi
@@ -125,50 +111,42 @@ start|"")
 
 	wait_for $RAM_LOG
 
-	log_action_begin_msg "Creating temporary system folders..."
+	echo "Creating temporary system folders..."
 	createFolders
-	log_action_end_msg 0
 
-	log_action_begin_msg "Restoring Syslog and LoxBerry system log folders..."
+	echo "Restoring Syslog and LoxBerry system log folders..."
 	cp -ra $LBHOMEDIR/log/skel_syslog/* /var/log
 	cp -ra $LBHOMEDIR/log/skel_system/* $LBHOMEDIR/log/system_tmpfs
-	log_action_end_msg 0
 
 	# Copy logdb from SD card to RAM disk
 	if [ -e $LBHOMEDIR/log/system/logs_sqlite.dat.bkp ]; then
-	        log_action_begin_msg "Copy back Backup of Logs SQLite Database"
+	        echo "Copy back Backup of Logs SQLite Database..."
 		cp -f $LBHOMEDIR/log/system/logs_sqlite.dat.bkp $LBHOMEDIR/log/system_tmpfs/logs_sqlite.dat
 		chown loxberry:loxberry $LBHOMEDIR/log/system_tmpfs/logs_sqlite.dat
 		chmod +rw $LBHOMEDIR/log/system_tmpfs/logs_sqlite.dat
-		log_action_end_msg 0
 	fi
 	
 	# Copy CloudDNS cache from SD card to RAM disk
 	if [ -e $LBHOMEDIR/log/system/clouddns_cache.bkp ]; then
-	        log_action_begin_msg "Copy back CloudDNS cache"
+	        echo "Copy back CloudDNS cache"
 		cp -f $LBHOMEDIR/log/system/clouddns_cache.bkp $LBHOMEDIR/log/system_tmpfs/clouddns_cache.json
 		chown loxberry:loxberry $LBHOMEDIR/log/system_tmpfs/clouddns_cache.json
 		chmod +rw $LBHOMEDIR/log/system_tmpfs/clouddns_cache.json
-		log_action_end_msg 0
 	fi
 
 	exit 0
-
 ;;
 
 stop)
-
 	# Copy logdb from RAM disk to SD card
 	if [ -e $LBHOMEDIR/log/system_tmpfs/logs_sqlite.dat ]; then
 		echo "VACUUM;" | sqlite3 $LBHOMEDIR/log/system_tmpfs/logs_sqlite.dat
 		sqlite3 $LBHOMEDIR/log/system_tmpfs/logs_sqlite.dat ".backup '$LBHOMEDIR/log/system/logs_sqlite.dat.bkp'"
-		# cp -f $LBHOMEDIR/log/system_tmpfs/logs_sqlite.dat $LBHOMEDIR/log/system/logs_sqlite.dat.bkp
 	fi
 	
 	if [ -e /run/shm/clouddns_cache.json ]; then
-        	log_action_begin_msg "Backup CloudDNS cache to SD"
+        	echo "Backup CloudDNS cache to SD..."
 		cp -f $LBHOMEDIR/log/system_tmpfs/clouddns_cache.json $LBHOMEDIR/log/system/clouddns_cache.bkp
-		log_action_end_msg 0
 	fi
 	
 	exit 0
