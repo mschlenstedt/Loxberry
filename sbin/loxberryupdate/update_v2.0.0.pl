a9ee4a79d25c44c33e1f0a9aca99904bfdc3e293
mschlenstedt <Michael@schlenn.net>
2019-10-03 14:52:59 +0200

    Update update_v2.0.0.pl

--- sbin/loxberryupdate/update_v2.0.0.pl
+++ sbin/loxberryupdate/update_v2.0.0.pl
@@ -415,7 +415,8 @@ LOGOK "Update script $0 finished." if ($errors == 0);
 LOGERR "Update script $0 finished with errors." if ($errors != 0);
 
 # End Skript for reboot
-exit(250); 
-
-# End of script
-exit($errors);
+if ($errors) {
+	exit(251); 
+} else {
+	exit(250);
+}
