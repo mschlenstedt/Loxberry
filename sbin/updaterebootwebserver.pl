aa1cb749f4c8bf7fd50fa31382a708c6375c6ae5
Michael Schlenstedt <michael@schlenn.net>
2019-10-09 05:12:41 +0200

    Updaterunning Flag; Fixes #1040

--- sbin/updaterebootwebserver.pl
+++ sbin/updaterebootwebserver.pl
@@ -27,19 +27,19 @@ my $logfile = shift;
 while (my $c = $d->accept) {
     while (my $r = $c->get_request) {
         if($r->method eq 'GET' && $r->uri->path eq "/lastlines") {
-			lastlines($c);
-		} elsif ($r->method eq 'GET' && $r->uri->path eq "/logfile") {
-            $c->send_file_response("$logfile");
-		} elsif ($r->method eq 'GET' && $r->uri->path eq "/system/scripts/jquery/jquery-1.12.4.min.js") {
-            $c->send_file_response("$lbshtmldir/scripts/jquery/jquery-1.12.4.min.js");
+		lastlines($c);
+	} elsif ($r->method eq 'GET' && $r->uri->path eq "/logfile") {
+        	$c->send_file_response("$logfile");
+	} elsif ($r->method eq 'GET' && $r->uri->path eq "/system/scripts/jquery/jquery-1.12.4.min.js") {
+        	$c->send_file_response("$lbshtmldir/scripts/jquery/jquery-1.12.4.min.js");
     	} elsif ($r->method eq 'GET' && $r->uri->path eq "/updatereboot-anim-large.gif") {
-			$c->send_file_response("$lbshtmldir/images/updatereboot-anim-large.gif");
-		} elsif ($r->method eq 'GET' && $r->uri->path eq "/admin/system/tools/power-handler.php") {
-			$c->send_file_response("$lbstemplatedir/reboot_updaterunning_ph.html");
-		} elsif ($r->method eq 'GET' && $r->uri->path eq "/updaterunning") {
-            $c->send_file_response("/tmp/updaterunning");
+		$c->send_file_response("$lbshtmldir/images/updatereboot-anim-large.gif");
+	} elsif ($r->method eq 'GET' && $r->uri->path eq "/admin/system/tools/power-handler.php") {
+		$c->send_file_response("$lbstemplatedir/reboot_updaterunning_ph.html");
+	} elsif ($r->method eq 'GET' && $r->uri->path eq "/updaterunning") {
+		$c->send_file_response("$lbstemplatedir/reboot_updaterunning_flag.html");
         } else {
-            $c->send_file_response("$lbstemplatedir/reboot_updaterunning.html");
+		$c->send_file_response("$lbstemplatedir/reboot_updaterunning.html");
         }
     }
     $c->close;
