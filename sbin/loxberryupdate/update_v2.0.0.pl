b9c2d8eef36eed3c9a715d03edfc3298a76d35d7
Christian Fenzl <fenzl@t-r-t.at>
2019-10-15 22:18:32 +0200

    update_v2.0.0.pl: Integrated plugindatabase migration

--- sbin/loxberryupdate/update_v2.0.0.pl
+++ sbin/loxberryupdate/update_v2.0.0.pl
@@ -399,27 +399,28 @@ system ("/bin/systemctl enable loxberry.service");
 system ("/bin/systemctl enable createtmpfs.service");
 
 if( -e $LoxBerry::System::PLUGINDATABASE ) {
-	print "<WARN> Plugin database is already migrated. Skipping.";
-} 
+	LOGWARN "Plugin database is already migrated. Skipping.";
+} else {
+	# # Migrate plugindatabase.dat to plugindatabase.json
+	# my ($exitcode, $output) = LoxBerry::System::execute( 
+		# log => $log,
+		# intro => "Migrating plugindatabase to json file format",
+		# command => "perl $lbhomedir/sbin/loxberryupdate/migrate_plugindb_v2.pl",
+		# ok => "Plugin database migrated successfully.",
+		# error => "Migration returned an error."
+	# );
 
-else {
-
-	# Migrate plugindatabase.dat to plugindatabase.json
-	my ($exitcode, $output) = LoxBerry::System::execute( 
-		log => $log,
-		intro => "Migrating plugindatabase to json file format",
-		command => "perl $lbhomedir/sbin/loxberryupdate/migrate_plugindb_v2.pl",
-		ok => "Plugin database migrated successfully.",
-		error => "Migration returned an error."
-	);
-	LOGDEB $output;
-	if($exitcode == 0) {
+	eval {
+		migration_plugindb();
+	};
+	if($@) {
+		$errors++;
+		LOGCRIT "Migration of plugindatabase failed: $@";
+		LOGWARN "Because of errors, the old files of plugindatabase are kept for further investigation.";
+	} else {
 		LOGINF "The old plugin database is kept for any issues as plugindatabase.dat";
 		unlink "$lbsdatadir/plugindatabase.dat-";
 		unlink "$lbsdatadir/plugindatabase.bkp";
-	} else {
-		$errors++;
-		LOGWARN "Because of errors, the old files of plugindatabase are kept for further investigation.";
 	}
 }
 
@@ -443,4 +444,160 @@ if ($errors) {
 	exit(251); 
 } else {
 	exit(250);
+}
+
+
+####################################################
+# Plugindatabase migration
+####################################################
+
+sub migration_plugindb 
+{
+
+	LOGINF "Migrating plugin database to LoxBerry 2.x format...";
+
+	use LoxBerry::System;
+	use LoxBerry::JSON;
+	use LoxBerry::System::PluginDB;
+
+	# $LoxBerry::JSON::DEBUG = 1;
+
+	if(!$lbsdatadir) {
+		die("Could not read LoxBerry variables");
+	}
+
+	my $plugin_count_old = 0;
+	my $plugin_count_new = 0;
+
+	# Create new database file
+	LOGINF "Deleting existing (new) plugindatabase.json";
+	unlink $LoxBerry::System::PLUGINDATABASE;
+
+	# Read old database with old code
+	my @plugins = get_plugins_V1();
+
+	my $plugin_count_old = scalar @plugins;
+
+	## Transform data
+	foreach $oldplugin ( @plugins ) {
+		LOGINF "Migrating plugin $oldplugin->{PLUGINDB_MD5_CHECKSUM} $oldplugin->{PLUGINDB_NAME} $oldplugin->{PLUGINDB_TITLE}";
+		
+		my $plugin = LoxBerry::System::PluginDB->plugin(
+			author_name => $oldplugin->{PLUGINDB_AUTHOR_NAME},
+			author_email => $oldplugin->{PLUGINDB_AUTHOR_EMAIL},
+			version => $oldplugin->{PLUGINDB_VERSION},
+			name => $oldplugin->{PLUGINDB_NAME},
+			folder => $oldplugin->{PLUGINDB_FOLDER},
+			title => $oldplugin->{PLUGINDB_TITLE},
+			interface => $oldplugin->{PLUGINDB_INTERFACE},
+			autoupdate => $oldplugin->{PLUGINDB_AUTOUPDATE},
+			releasecfg => $oldplugin->{PLUGINDB_RELEASECFG},
+			prereleasecfg => $oldplugin->{PLUGINDB_PRERELEASECFG},
+			loglevel => $oldplugin->{PLUGINDB_LOGLEVEL},
+			loglevels_enabled => $oldplugin->{PLUGINDB_LOGLEVELS_ENABLED},
+		);
+		
+		if($plugin) {
+			if( $plugin->{md5} ne $oldplugin->{PLUGINDB_MD5_CHECKSUM} ) {
+				LOGWARN "  Newly calculated pluginid does not match old pluginid";
+			} else {
+				LOGOK "  Created database entry for $plugin->{title}";
+			}
+			$plugin->save;
+		} else {
+			LOGERR "  Could not create entry for $oldplugin->{PLUGINDB_TITLE}";
+		}
+	}
+
+	LOGINF "Checking new database file...";
+	eval {
+		$data = JSON::from_json( LoxBerry::System::read_file( $LoxBerry::System::PLUGINDATABASE ) );
+	};
+	if($data and $data->{plugins}) {
+		$plugin_count_new = scalar keys %{$data->{plugins}};
+	} else {
+		$plugin_count_new = 0;
+	}
+
+	LOGINF "Creating backup and shadow copy of plugindatabase";
+	`cp -f $Loxberry::System::PLUGINDATABASE $Loxberry::System::PLUGINDATABASE-`;
+	`cp -f $Loxberry::System::PLUGINDATABASE $Loxberry::System::PLUGINDATABASE.bkp`;
+	`chmod 644 $Loxberry::System::PLUGINDATABASE-`;
+	`chown root:root $Loxberry::System::PLUGINDATABASE-`;
+
+	LOGINF "Number of plugins BEFORE: " . $plugin_count_old;
+	LOGINF "Number of plugins AFTER : " . $plugin_count_new;
+
+	if( $plugin_count_old ne $plugin_count_new ) {
+		LOGERR "Plugin count is not equal. Check the list above. Before trying any plugin installations, contact the LoxBerry-Core team via https://loxforum.com or https://github.com/mschlenstedt/Loxberry/issues";
+		die("Plugin count BEFORE and AFTER is not equal");
+	} else {
+		LOGOK "Plugindatabase migration finished successfully.";
+	}
+
+	return(0);
+}
+
+
+# This is the code to read the old plugindatabase.dat
+sub get_plugins_V1
+{
+		
+	if (! $plugindb_file) {
+		$plugindb_file = "$lbsdatadir/plugindatabase.dat";
+	}
+	print STDERR "get_plugins: Using file $plugindb_file\n" if ($DEBUG);
+	
+	if (!-e $plugindb_file) {
+		Carp::carp "LoxBerry::System::pluginversion: Could not find $plugindb_file\n";
+		return;
+	}
+	my $openerr;
+	open(my $fh, "<", $plugindb_file) or ($openerr = 1);
+	if ($openerr) {
+		Carp::carp "Error opening plugin database $plugindb_file";
+		# &error;
+		return;
+		}
+	my @data = <$fh>;
+
+	my @plugins = ();
+	my $plugincount = 0;
+	
+	foreach (@data){
+		s/[\n\r]//g;
+		my %plugin;
+		# Comments
+		if ($_ =~ /^\s*#.*/) {
+			if (defined $withcomments) {
+				$plugin{PLUGINDB_COMMENT} = $_;
+				push(@plugins, \%plugin);
+			}
+			next;
+		}
+		
+		$plugincount++;
+		my @fields = split(/\|/);
+
+		# From Plugin-DB
+		
+		$plugin{PLUGINDB_NO} = $plugincount;
+		$plugin{PLUGINDB_MD5_CHECKSUM} = $fields[0];
+		$plugin{PLUGINDB_AUTHOR_NAME} = $fields[1];
+		$plugin{PLUGINDB_AUTHOR_EMAIL} = $fields[2];
+		$plugin{PLUGINDB_VERSION} = $fields[3];
+		$plugin{PLUGINDB_NAME} = $fields[4];
+		$plugin{PLUGINDB_FOLDER} = $fields[5];
+		$plugin{PLUGINDB_TITLE} = $fields[6];
+		$plugin{PLUGINDB_INTERFACE} = $fields[7];
+		$plugin{PLUGINDB_AUTOUPDATE} = $fields[8];
+		$plugin{PLUGINDB_RELEASECFG} = $fields[9];
+		$plugin{PLUGINDB_PRERELEASECFG} = $fields[10];
+		$plugin{PLUGINDB_LOGLEVEL} = $fields[11];
+		$plugin{PLUGINDB_LOGLEVELS_ENABLED} = $plugin{PLUGINDB_LOGLEVEL} >= 0 ? 1 : 0;
+		$plugin{PLUGINDB_ICONURI} = "/system/images/icons/$plugin{PLUGINDB_FOLDER}/icon_64.png";
+		push(@plugins, \%plugin);
+
+	}
+	return @plugins;
 }
