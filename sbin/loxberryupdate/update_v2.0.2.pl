fd3f4cd8890d7bb5846850365d0c23399efbe06c
Michael Schlenstedt <Michael@schlenn.net>
2020-05-04 19:25:19 +0200

    Add additional servers for apt

--- sbin/loxberryupdate/update_v2.0.2.pl
+++ sbin/loxberryupdate/update_v2.0.2.pl
@@ -14,6 +14,14 @@ init();
 LOGINF "Deleting CloudDNS cache files to get rebuilt with https...";
 unlink "$lbstmpfslogdir/clouddns_cache.json";
 
+# Install additional apt sources e.g. if a server is down
+LOGINF "Add additional servers for the apt repositories...";
+open(FH, ">>", "/etc/apt/sources.list");
+print FH "deb http://ftp.gwdg.de/pub/linux/debian/raspbian/raspbian/ buster main contrib non-free rpi\n";
+print FH "deb http://ftp.agdsn.de/pub/mirrors/raspbian/raspbian/ buster main contrib non-free rpi\n";
+close(FH);
+
+#
 ## If this script needs a reboot, a reboot.required file will be created or appended
 # LOGWARN "Update file $0 requests a reboot of LoxBerry. Please reboot your LoxBerry after the installation has finished.";
 # reboot_required("LoxBerry Update requests a reboot.");
