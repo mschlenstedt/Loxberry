9234be511d4c0c8df67c4462fbc58bba8ac384da
Christian Fenzl <fenzl@t-r-t.at>
2020-11-13 08:16:44 +0100

    Update gruenland.php

--- webfrontend/html/XL/examples/gruenland.php
+++ webfrontend/html/XL/examples/gruenland.php
@@ -6,7 +6,8 @@ require_once "loxberry_json.php";
 /* 
 	LOXBERRY XL
 	EXtended Logic
-	 
+	
+	Grünlandtemperatursumme V2
 */
 
 $outtemp = $ms1->Außentemperatursensor;
@@ -45,12 +46,16 @@ if( !empty($cache->dailyavgtimestamp->year) && $cache->dailyavgtimestamp->year !
 // Make Daily Avg. calculation
 daily_average();
 
+$json_data = json_encode($cache, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_LINE_TERMINATORS | JSON_PRETTY_PRINT );
+
 // Send data
-$mqtt->retain( $topic, json_encode($cache) );
+$mqtt->retain( $topic, $json_data );
 
 // print_r($cache);
 
-file_put_contents($cachefile, json_encode($cache, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
+file_put_contents( $cachefile, $json_data );
+
+echo $json_data . "\n";
 
 // End
 
@@ -65,22 +70,21 @@ function daily_average() {
 
 	// Init variables
 	$cache->dailyavg = empty($cache->dailyavg) ? 0 : $cache->dailyavg;
-	$cache->dailyavg_count = empty($cache->dailyavg_count) ? 0 : $cache->dailyavg_count;
-	$cache->dailyavg_positive = empty($cache->dailyavg_positive) ? 0 : $cache->dailyavg_positive;
-	$cache->dailyavg_positive_count = empty($cache->dailyavg_positive_count) ? 0 : $cache->dailyavg_positive_count;
-
+	$cache->dailyavgCount = empty($cache->dailyavgCount) ? 0 : $cache->dailyavgCount;
+	
 	if( empty($outtemp) ) {
 		error_log("Außentemperatur nicht lesbar!");
 		exit(2);
 	}
 		
-	$cache->dailyavg = ( $cache->dailyavg * $cache->dailyavg_count + $outtemp ) / ($cache->dailyavg_count+1);
-	$cache->dailyavg_count++;
+	$cache->dailyavg = ( $cache->dailyavg * $cache->dailyavgCount + $outtemp ) / ($cache->dailyavgCount+1);
+	$cache->dailyavgCount++;
 	
-	if( $outtemp > 0 ) {
-		
-		$cache->dailyavg_positive = ( $cache->dailyavg_positive * $cache->dailyavg_positive_count + $outtemp ) / ($cache->dailyavg_positive_count+1);
-		$cache->dailyavg_positive_count++;
+	// Estimated (rolling) gts
+	$monthfactor = monthfactor();
+	$cache->gts->gtsest = empty( $cache->gts->gts ) ? 0 : $cache->gts->gts;
+	if( !empty( $cache->dailyavg ) && $cache->dailyavg > 0 ) {
+		$cache->gts->gtsest += $cache->dailyavg * $monthfactor;
 	}
 	
 	if( !property_exists($cache, "dailyavgtimestamp") ) {
@@ -89,8 +93,8 @@ function daily_average() {
 	$cache->dailyavgtimestamp->dayofyear = $xl->dayofyear;
 	$cache->dailyavgtimestamp->month = $xl->month;
 	$cache->dailyavgtimestamp->year = $xl->year;
-	$cache->dailyavgtimestamp->date_hr = $xl->date;
-	$cache->dailyavgtimestamp->time_hr = $xl->time;
+	$cache->dailyavgtimestamp->dateHR = $xl->date;
+	$cache->dailyavgtimestamp->timeHR = $xl->time;
 	$cache->dailyavgtimestamp->epoch = time();
 	
 }
@@ -103,6 +107,39 @@ function calculate_gts() {
 	
 	error_log("calculate_gts called");
 	
+	$monthfactor = monthfactor();
+	
+	$cache->gts->gts = empty($cache->gts->gts) ? 0 : $cache->gts->gts;
+	
+	if( !empty( $cache->dailyavg ) && $cache->dailyavg > 0 ) {
+		$cache->gts->gts = $cache->gts->gts + $cache->dailyavg * $monthfactor;
+	}
+	$cache->gts->dateHR = $xl->date;
+	$cache->gts->timeHR = $xl->time;
+	
+	// Write history file
+	$history = json_decode(file_get_contents($historyfile));
+	$year = $cache->dailyavgtimestamp->year;
+	$date = $cache->dailyavgtimestamp->dateHR;
+	$history->$year->$date->gts = $cache->gts->gts;
+	$history->$year->$date->dailyavg = $cache->dailyavg;
+	$history->$year->$date->dailyavgCount = $cache->dailyavgCount;
+	$history->$year->$date->dateHR = $xl->date;
+	$history->$year->$date->timeHR = $xl->time;
+	file_put_contents( $historyfile, json_encode($history, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) );
+
+	// Empty data after each day
+	unset(
+		$cache->dailyavg,
+		$cache->dailyavgCount,
+		$cache->dailyavg_positive,	
+		$cache->dailyavg_positive_count,
+	);
+	
+}
+
+function monthfactor() {
+	global $cache;
 	// Multiply factor
 	if( $cache->dailyavgtimestamp->month == 1 ) {
 		$month_factor = 0.5;
@@ -111,30 +148,8 @@ function calculate_gts() {
 	} else {
 		$month_factor = 1;
 	}
-	$cache->gts->gts = empty($cache->gts->gts) ? 0 : $cache->gts->gts;
-	$cache->gts->gts = $cache->gts->gts +  $cache->dailyavg_positive;
-	$cache->gts->date_hr = $xl->date;
-	$cache->gts->time_hr = $xl->time;
-	
-	// Write history file
-	$history = json_decode(file_get_contents($historyfile));
-	$year = $cache->dailyavgtimestamp->year;
-	$date = $cache->dailyavgtimestamp->date_hr;
-	$history->$year->$date->gts = $cache->gts->gts;
-	$history->$year->$date->dailyavg = $cache->dailyavg;
-	$history->$year->$date->dailyavg_count = $cache->dailyavg_count;
-	$history->$year->$date->dailyavg_positive = $cache->dailyavg_positive;
-	$history->$year->$date->dailyavg_positive_count = $cache->dailyavg_positive_count;
-	$history->$year->$date->date_hr = $xl->date;
-	$history->$year->$date->time_hr = $xl->time;
-	file_put_contents( $historyfile, json_encode($history, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) );
-
-	// Empty data after each day
-	unset(
-		$cache->dailyavg,
-		$cache->dailyavg_count,
-		$cache->dailyavg_positive,	
-		$cache->dailyavg_positive_count,
-	);
-	
+	return $month_factor;
 }
+	
+	
+	
