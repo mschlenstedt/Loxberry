b44d3c34bb77c4ae2d66fb62250092a916c0a729
MIchael Schlenstedt <Michael@schlenn.net>
2020-12-28 08:56:28 +0100

    Add missing /boot/config.txt and /boot/cmdline.txt from broken Image loxberry-image-rasppi-2.2.0b_pippin

---
+++ sbin/loxberryupdate/update_v2.2.0.5.pl
@@ -0,0 +1,107 @@
+#!/usr/bin/perl
+
+# Input parameters from loxberryupdate.pl:
+# 	release: the final version update is going to (not the version of the script)
+#   logfilename: The filename of LoxBerry::Log where the script can append
+#   updatedir: The directory where the update resides
+#   cron: If 1, the update was triggered automatically by cron
+
+use LoxBerry::Update;
+
+init();
+
+
+# Install missing /boot/config.txt and cmdline.txt from broken Image loxberry-image-rasppi-2.2.0b_pippin.img.7z
+LOGINF "Checking for /boot/cmdline.txt and /boot/config.txt...";
+if ( !-e "/boot/cmdline.txt" || !-e "/boot/config.txt" ) {
+	LOGINF "One of the two files is missing. Reinstalling...";
+	if ( !-e "/boot/cmdline.txt" ) {
+	open(F,">/boot/cmdline.txt");
+	print F "dwc_otg.lpm_enable=0 console=serial0,115200 console=tty1 root=PARTUUID=4bd27daf-02 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait";
+	close (F);
+	}
+	if ( !-e "/boot/config.txt" ) {
+	open(F,">/boot/config.txt");
+	print F <<EOF;
+# For more options and information see
+# http://rpf.io/configtxt
+# Some settings may impact device functionality. See link above for details
+
+# uncomment if you get no picture on HDMI for a default "safe" mode
+hdmi_safe=1
+
+# uncomment this if your display has a black border of unused pixels visible
+# and your display can output without overscan
+#disable_overscan=1
+
+# uncomment the following to adjust overscan. Use positive numbers if console
+# goes off screen, and negative if there is too much border
+#overscan_left=16
+#overscan_right=16
+#overscan_top=16
+#overscan_bottom=16
+
+# uncomment to force a console size. By default it will be display's size minus
+# overscan.
+#framebuffer_width=1280
+#framebuffer_height=720
+
+# uncomment if hdmi display is not detected and composite is being output
+hdmi_force_hotplug=1
+
+# uncomment to force a specific HDMI mode (this will force VGA)
+#hdmi_group=1
+#hdmi_mode=1
+
+# uncomment to force a HDMI mode rather than DVI. This can make audio work in
+# DMT (computer monitor) modes
+#hdmi_drive=2
+
+# uncomment to increase signal to HDMI, if you have interference, blanking, or
+# no display
+config_hdmi_boost=4
+
+# uncomment for composite PAL
+#sdtv_mode=2
+
+#uncomment to overclock the arm. 700 MHz is the default.
+#arm_freq=800
+
+# Uncomment some or all of these to enable the optional hardware interfaces
+dtparam=i2c1=on
+dtparam=i2c_arm=on
+#dtparam=i2s=on
+#dtparam=spi=on
+
+# Uncomment this to enable the lirc-rpi module
+#dtoverlay=lirc-rpi
+
+# Additional overlays and parameters are documented /boot/overlays/README
+
+# Enable audio (loads snd_bcm2835)
+dtparam=audio=on
+
+[pi4]
+# Enable DRM VC4 V3D driver on top of the dispmanx display stack
+dtoverlay=vc4-fkms-v3d
+max_framebuffers=2
+
+[all]
+#dtoverlay=vc4-fkms-v3d
+EOF
+	close (F);
+	}
+} else {
+	LOGINF "Files exist. Fine. Nothing to do.";
+}
+
+
+## If this script needs a reboot, a reboot.required file will be created or appended
+#LOGWARN "Update file $0 requests a reboot of LoxBerry. Please reboot your LoxBerry after the installation has finished.";
+#reboot_required("LoxBerry Update requests a reboot.");
+
+LOGOK "Update script $0 finished." if ($errors == 0);
+LOGERR "Update script $0 finished with errors." if ($errors != 0);
+
+# End of script
+exit($errors);
