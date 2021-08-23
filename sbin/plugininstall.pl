1f9145b1fef44ce306f48fd3f234c41798c80c6e
mschlenstedt <Michael@schlenn.net>
2021-08-23 14:01:51 +0200

    Correct logging

--- sbin/plugininstall.pl
+++ sbin/plugininstall.pl
@@ -1253,7 +1253,7 @@ sub install {
 			$message = "Command: $export $aptbin -y -q --allow-unauthenticated --fix-broken --reinstall --allow-downgrades --allow-remove-essential --allow-change-held-packages --purge autoremove";
 			&loginfo;
 			system("$export $aptbin -y -q --allow-unauthenticated --fix-broken --reinstall --allow-downgrades --allow-remove-essential --allow-change-held-packages --purge autoremove 2>&1");
-			$message = "Command: $export $aptbin -q -y --allow-unauthenticated --allow-downgrades --allow-remove-essential --allow-change-held-packages update";
+			$message = "Command: $export $aptbin -q -y --allow-unauthenticated --allow-downgrades --allow-remove-essential --allow-change-held-packages --allow-releaseinfo-change update";
 			&loginfo;
 			system("$export $aptbin -q -y --allow-unauthenticated --allow-downgrades --allow-remove-essential --allow-change-held-packages --allow-releaseinfo-change update 2>&1");
 			if ($? ne 0) {
