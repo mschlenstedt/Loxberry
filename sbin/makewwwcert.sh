891d0be8f3e71c1e4101b919f29a2ccd75960ab0
Svethi <svethi@loxberry.de>
2022-11-15 21:32:25 +0100

    SSL Beta2

--- sbin/makewwwcert.sh
+++ sbin/makewwwcert.sh
@@ -4,6 +4,7 @@ CATOP="$LBHOMEDIR/data/system/LoxBerryCA"
 cd "$CATOP/newcerts"
 $LBHOMEDIR/sbin/CA.pl -newreq
 $LBHOMEDIR/sbin/CA.pl -sign
+cp ../cacert.pem $LBHOMEDIR/webfrontend/html/system/cacert.pem
 mv newcert.pem $CATOP/certs/wwwcert.pem
 mv newkeywp.pem $CATOP/private/wwwkeywp.pem
 mv newkey.pem $CATOP/private/wwwkey.pem
