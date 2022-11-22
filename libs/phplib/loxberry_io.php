e908ee81541b3877a8e3d8eed40109ea7142eb41
Christian Fenzl <fenzl@t-r-t.at>
2022-11-22 23:00:43 +0100

    loxberry_io.php: Updated mqtt_connectiondetails()
    To use config from general.json

--- libs/phplib/loxberry_io.php
+++ libs/phplib/loxberry_io.php
@@ -6,7 +6,7 @@ $mem_sendall_sec = 3600;
 $mem_sendall = 0;
 $udp_delimiter = '=';
 
-$LBIOVERSION = "2.2.1.4";
+$LBIOVERSION = "3.0.0.1";
 
 // msudp_send
 function msudp_send($msnr, $udpport, $prefix, $params)
@@ -445,37 +445,23 @@ function mshttp_send_mem($msnr, $params, $value = null)
 
 // Read MQTT connection details and credentials from MQTT plugin
 function mqtt_connectiondetails() {
-
-	# Check if MQTT Gateway plugin is installed
-	$mqttplugindata = LBSystem::plugindata("mqttgateway");
-	$pluginfolder = $mqttplugindata['PLUGINDB_FOLDER'];
-	if(empty($pluginfolder)) {
-		return;
-	}
-
-	// $mqttconf;
-    // $mqttcred;
 	
-		# Read connection details
-		$mqttconf = json_decode(file_get_contents(LBHOMEDIR . "/config/plugins/" . $pluginfolder . "/mqtt.json" ));
-		$mqttcred = json_decode(file_get_contents(LBHOMEDIR . "/config/plugins/" . $pluginfolder . "/cred.json" ));
-
-	if( $mqttconf === FALSE || $mqttcred === FALSE ) {
-		error_log("loxberry_io connectiondetails: Failed to read/parse connection details");
-		return;
-	}
+	global $cfgwasread;
+	global $cfg;
+	
+	if (! isset($cfgwasread)) {
+			LBSystem::read_generaljson();
+	}	
 	
 	$cred = array ();
 	
-	@list($brokerhost, $brokerport) = explode(':', $mqttconf->{'Main'}->{'brokeraddress'}, 2);
-	$brokerport = $brokerport ? $brokerport : "1883";
-	$cred['brokeraddress'] = $brokerhost.":".$brokerport;
-	$cred['brokerhost'] = $brokerhost;
-	$cred['brokerport'] = $brokerport;
-	$cred['websocketport'] = !empty($mqttconf->Main->websocketport) ? $mqttconf->Main->websocketport : "9001";
-	$cred['brokeruser'] = $mqttcred->Credentials->brokeruser;
-	$cred['brokerpass'] = $mqttcred->Credentials->brokerpass;
-	$cred['udpinport'] = $mqttconf->Main->udpinport;
+	$cred['brokeraddress'] = $cfg->Mqtt->Brokerhost.":".$cfg->Mqtt->Brokerport;
+	$cred['brokerhost'] = $cfg->Mqtt->Brokerhost;
+	$cred['brokerport'] = $cfg->Mqtt->Brokerport;
+	$cred['websocketport'] = !empty($cfg->Mqtt->Websocketport) ? $cfg->Mqtt->Websocketport : "9001";
+	$cred['brokeruser'] = $cfg->Mqtt->Brokeruser;
+	$cred['brokerpass'] = $cfg->Mqtt->Brokerpass;
+	$cred['udpinport'] = $cfg->Mqtt->Udpinport;
 	return $cred;
 
 }
