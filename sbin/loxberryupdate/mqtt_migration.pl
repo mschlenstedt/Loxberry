be137dcbaa3609b16372bae76e93d33c91fc5cd0
mschlenstedt <Michael@schlenn.net>
2023-03-03 18:03:20 +0100

    Set Permissions after Migration

--- sbin/loxberryupdate/mqtt_migration.pl
+++ sbin/loxberryupdate/mqtt_migration.pl
@@ -221,8 +221,14 @@ sub set_file_permissions
 	###
 	### Set file permissions
 	###
-	
-	
+	execute( command => "chown -R loxberry:loxberry $lbhomedir/bin/mqtt/transform/custom", log => $log, ignoreerrors => 1 );
+	execute( command => "chown -R loxberry:loxberry $lbhomedir/bin/mqtt/datastore", log => $log, ignoreerrors => 1 );
+	execute( command => "chown -R loxberry:loxberry $lbhomedir/config/system/mosquitto", log => $log, ignoreerrors => 1 );
+	execute( command => "chown -R loxberry:loxberry $lbsconfigdir/mqttgateway.json", log => $log, ignoreerrors => 1 );
+	execute( command => "chown -R loxberry:loxberry /opt/backup.mqttgateway", log => $log, ignoreerrors => 1 );
+	execute( command => "chown -R loxberry:loxberry $lbhomedir/webfrontend/htmlauth/plugins/mqttgateway", log => $log, ignoreerrors => 1 );
+	execute( command => "chown -R loxberry:loxberry $lbhomedir/webfrontend/html/plugins/mqttgateway", log => $log, ignoreerrors => 1 );
+	execute( command => "chown -R loxberry:loxberry $lbsconfigdir/general.json", log => $log, ignoreerrors => 1 );
 }
 
 sub start_mqttgateway
