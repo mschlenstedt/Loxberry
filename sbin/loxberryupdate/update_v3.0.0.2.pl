2edab51d386d2a5ab222cec5b2321c05d5124a48
mschlenstedt <Michael@schlenn.net>
2023-03-21 06:02:12 +0100

    Update update_v3.0.0.2.plDele ~/storage/usb if it is a folder

--- sbin/loxberryupdate/update_v3.0.0.2.pl
+++ sbin/loxberryupdate/update_v3.0.0.2.pl
@@ -17,7 +17,8 @@ unlink ("/etc/php/7.4/cli/conf.d/20-loxberry-cli.ini");
 execute( command => "ln -vsfn $lbhomedir/system/php/loxberry-cli.ini /etc/php/7.4/cli/conf.d/20-loxberry-cli.ini", log => $log );
 
 LOGINF "Repairing broken Symlink for USB mount";
-unlink ("$lbhomedir/system/storage/usb");
+#unlink ("$lbhomedir/system/storage/usb");
+execute( command => "rm -r $lbhomedir/system/storage/usb", log => $log );
 execute( command => "ln -vsfn /media/usb $lbhomedir/system/storage/usb", log => $log );
 execute( command => "chown -h loxberry:loxberry $lbhomedir/system/storage/usb", log => $log );
 
