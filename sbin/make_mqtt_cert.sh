165b2e299ed825abf762211c816195fb2b29bfb1
nufke <nufke@barnasconi.net>
2023-12-22 23:29:12 +0100

    update access rights for certs

--- sbin/make_mqtt_cert.sh
+++ sbin/make_mqtt_cert.sh
@@ -2,10 +2,13 @@
 
 CA_DIR="$LBHOMEDIR/data/system/LoxBerryCA"
 MOSQ_DIR="$LBHOMEDIR/data/system/mosquitto"
-mkdir $MOSQ_DIR
+mkdir $MOSQ_DIR/certs/private
 cp $CA_DIR/cacert.pem $MOSQ_DIR/certs/cacert.pem
 cp $CA_DIR/certs/wwwcert.pem $MOSQ_DIR/certs/mqtt_server.pem
 cp $CA_DIR/private/wwwkeywp.pem $MOSQ_DIR/certs/private/mqtt_server_key.pem
+chmod 600 $MOSQ_DIR/certs/cacert.pem
+chmod 600 $MOSQ_DIR/certs/mqtt_server.pem
+chmod 600 $MOSQ_DIR/certs/private/mqtt_server_key.pem
 chown mosquitto $MOSQ_DIR/certs/cacert.pem
 chown mosquitto $MOSQ_DIR/certs/mqtt_server.pem
 chown mosquitto $MOSQ_DIR/certs/private/mqtt_server_key.pem
