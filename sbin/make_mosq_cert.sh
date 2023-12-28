9686bf6301920b135fbd7048c538508d0260e244
nufke <nufke@barnasconi.net>
2023-12-28 17:30:11 +0100

    fix openssl cmd

--- sbin/make_mosq_cert.sh
+++ sbin/make_mosq_cert.sh
@@ -5,7 +5,7 @@ MOSQ_DIR="$LBHOMEDIR/data/system/mosquitto"
 mkdir -p $MOSQ_DIR
 mkdir $MOSQ_DIR/newcerts $MOSQ_DIR/certs $MOSQ_DIR/private
 cd $MOSQ_DIR/newcerts
-openssl req -new -out newreq.pem -outkey newkey.pem -subj='/C=/ST=/L=/O=/OU=/CN=loxberrymqtt' -passout $CA_DIR/randb64
+openssl req -new -subj='/C=/ST=/L=/O=/OU=/CN=loxberrymqtt' -keyout newkey.pem -out newreq.pem  -passout file:$LBHOMEDIR/data/system/LoxBerryCA/randb64
 $LBHOMEDIR/sbin/CA.pl -sign
 mv newcert.pem $MOSQ_DIR/certs/mosqcert.pem
 mv newkeywp.pem $MOSQ_DIR/private/mosqkeywp.pem
